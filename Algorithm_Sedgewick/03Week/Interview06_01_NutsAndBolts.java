c0827e63f09182792543be94351bedde1e4ca271
jhkim988 <fccva3910@gmail.com>
2021-03-13 11:56:16 +0900

    Algorithm Learn Interview0601

--- Algorithm_Sedgewick/03Week/Interview06_01_NutsAndBolts.java
+++ Algorithm_Sedgewick/03Week/Interview06_01_NutsAndBolts.java
@@ -18,13 +18,13 @@ public class Interview06_01_NutsAndBolts {
 		Bolt(int id) {
 			this.id = id;
 		}
-		
+
 		int id() {
 			return id;
 		}
-		
-		int compares (Nut nut) {
-			if(id() > nut.id()) {
+
+		int compares(Nut nut) {
+			if (id() > nut.id()) {
 				return 1;
 			} else if (id() < nut.id()) {
 				return -1;
@@ -32,22 +32,25 @@ public class Interview06_01_NutsAndBolts {
 				return 0;
 			}
 		}
-		
+
 		public String toString() {
-			return id+" ";
+			return id + " ";
 		}
 	}
+
 	private static class Nut {
 		private int id;
+
 		int id() {
 			return id;
 		}
+
 		Nut(int id) {
 			this.id = id;
 		}
-		
-		int compares (Bolt bolt ) {
-			if(id() > bolt.id()) {
+
+		int compares(Bolt bolt) {
+			if (id() > bolt.id()) {
 				return 1;
 			} else if (id() < bolt.id()) {
 				return -1;
@@ -55,55 +58,76 @@ public class Interview06_01_NutsAndBolts {
 				return 0;
 			}
 		}
-		
+
 		public String toString() {
-			return id+" ";
+			return id + " ";
 		}
-	}	
+	}
+
 	public static Bolt[] randomBoltArrayGenerator(int size) {
 		// Bolt id range : 0 ~ size - 1
 		Bolt[] resultArr = new Bolt[size];
-		for(int i = 0; i < size; i++) {
+		for (int i = 0; i < size; i++) {
 			resultArr[i] = new Bolt(i);
 		}
 		StdRandom.shuffle(resultArr);
 		return resultArr;
 	}
-	
+
 	public static Nut[] randomNutArrayGenerator(int size) {
 		// Bolt id range : 0 ~ size - 1
 		Nut[] resultArr = new Nut[size];
-		for(int i = 0; i < size; i++) {
+		for (int i = 0; i < size; i++) {
 			resultArr[i] = new Nut(i);
 		}
 		StdRandom.shuffle(resultArr);
 		return resultArr;
 	}
-	
-	public static void findPair(Bolt[] bolts, Nut[] nuts, int lo, int hi) {
+
+	private static void exch(Object[] arr, int idx1, int idx2) {
+		Object tmp = arr[idx1];
+		arr[idx1] = arr[idx2];
+		arr[idx2] = tmp;
+	}
+
+	private static int findPair(Bolt[] bolts, Nut[] nuts, int lo, int hi) {
 		// only use compare() method, NOT id()
 		// nlogn algorithm
-		
+
 		// move bolts[]. fixed nuts[].
 		// two points, lo and hi, imitate quick sort(selection) algorithm.
 		// use the fact that all are distinct.
-		
-		// partitioning [ < nuts[lo] | = nuts[lo] | > nuts[lo] ]
+
+		// partitioning [ < nuts[lo] | = nuts[lo](only one) | > nuts[lo] ]
 		// return i such that nuts[lo].compares(bolts[i]) == 0;
 		int i = lo, j = hi + 1;
-		while(i <= j) {
-			if (bolts[i].compares(nuts[lo]) < 0) i++;
-			else if (bolts[i].compares(nuts[lo]) > 0) exch(bolts, i, j--);
-			else exch(bolts, i++, lo++);
-		}
-		
+		while (i <= j) {
+			if (bolts[i].compares(nuts[lo]) < 0)
+				i++;
+			else if (bolts[i].compares(nuts[lo]) > 0)
+				exch(bolts, i, j--);
+			else
+				exch(bolts, lo, i++);
+		} // bolts[lo] - nuts[lo] fit exactly
+		return j;
 	}
-	
+
+	public static void findPair(Bolt[] bolts, Nut[] nuts) {
+		int size = bolts.length;
+		assert (size == nuts.length);
+		
+		int nutsIdx = 0;
+		int boundary = findPair(bolts, nuts, nutsIdx, size - 1);
+		if (nuts[nutsIdx + 1].compares(bolts[nutsIdx]) < 0) {
+			
+		}
+	}
+
 	public static void main(String[] args) {
 		int size = 100;
 		Bolt[] boltArr = randomBoltArrayGenerator(size);
 		Nut[] nutArr = randomNutArrayGenerator(size);
-		
+
 		StdOut.println(Arrays.toString(boltArr));
 		StdOut.println(Arrays.toString(nutArr));
 	}
