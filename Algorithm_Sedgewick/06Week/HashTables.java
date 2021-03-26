807ee6c8299971e2116d794d053e6a40830afdc0
jhKim988 <fccva3910@gmail.com>
2021-03-26 13:14:55 +0900

    06Week

---
+++ Algorithm_Sedgewick/06Week/HashTables.java
@@ -0,0 +1,12 @@
+public class HashTables {
+	// Symbolic Table --Hash function--> Array
+	
+	// Java's Hash code convention
+	// Requirement
+	// if (x.equals(y)), then (x.hashCode == y.hashCode)
+	// Highly Desirable
+	// if (!x.equals(y)), then (x.hashCode != y.hashCode)
+	
+	// Java hashCode: 32bit integer
+	// Default impletation: memory address of x
+}
