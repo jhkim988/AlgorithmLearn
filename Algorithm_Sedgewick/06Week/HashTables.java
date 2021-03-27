11f5eb58367c0922f10810d53fc5a3cc6aa68e79
jhkim988 <fccva3910@gmail.com>
2021-03-27 22:46:22 +0900

    Algorithm 06Week

--- Algorithm_Sedgewick/06Week/HashTables.java
+++ Algorithm_Sedgewick/06Week/HashTables.java
@@ -1,3 +1,5 @@
+import edu.princeton.cs.algs4.Date;
+
 public class HashTables {
 	// Symbolic Table --Hash function--> Array
 	
@@ -9,4 +11,54 @@ public class HashTables {
 	
 	// Java hashCode: 32bit integer
 	// Default impletation: memory address of x
+	
+	// table position must be uniformly distritubed
+	
+	// User-Defined Types - Standard Recipe
+	// 31*x + y rule
+	// primitive type: Use Wrapper type hashCode()
+	// field is null: 0
+	// field is reference type: use hashCode()
+	// field is an array, apply to each entry
+	private final class Transaction implements Comparable<Transaction> {
+		private final String who;
+		private final Date when;
+		private final double amount;
+		public Transaction(String who, Date when, double amount) {
+			this.who = who;
+			this.when = when;
+			this.amount = amount;
+		}
+		@Override
+		public int compareTo(HashTables.Transaction o) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+		public boolean equals(Object y) {
+			return true;
+		}
+		public int hashCode() {
+			int hash = 17; // nonzero constant(small prime number)
+			hash = 31 * hash + who.hashCode(); // Honer's method
+			hash = 31*hash + when.hashCode();
+			hash = 31*hash + ((Double) amount).hashCode();
+			return hash; // 32-bit integer
+		}
+	}
+	
+	// universal hash functions exist
+	// Need to use the whole key to compute hash code
+	
+	// Hash code: -2^31 ~ 2^31 - 1
+	// Hash function: 0 ~ M-1 (for use as array index)
+	// private int hash(Key key) { return key.hashCode() % M; } // Bug
+	// private int hash(Key key) { return Math.abs(key.hashCode()) % M; } // 1-in-a-billion bug (-2^31)
+	// private int hash(Key key) { return (key.hashCode() & 0x7fffffff) % M; } // correct
+	
+	// Uniform hashing assumption
+	// each key is equally likely to hash to an integer between 0 and M - 1
+	// Bins and balls - Throw balls uniformly at random into M bins
+	// Expect two balls in the same bin after ~ sqrt(pi * M / 2) tosses.(birthday problem)
+	// Expect every bin has >= 1 ball after M ln M tosses.(coupon collector problem)
+	// After M tosses, expect most loaded bin has theta(log M / log(log M)) balls(load balancing)
 }
---
+++ Algorithm_Sedgewick/06Week/SeperateChaining.java
@@ -0,0 +1,30 @@
+
+public class SeperateChaining {
+	// Collision: Two distinct keys hashing to same index
+	// Birthday problem - can't avoid collisions unless you have a ridiculous (quadratic) amount of memeory.
+	// Coupon collector + load balancing - collisions will be evenly distritubed.
+	// Challenge: Deal with collision efficiently.
+	// hash("it") = 3, hash("times") = 3
+	
+	// Use an array of M < N linked list
+	// Hash: map key to integer i betwwen 0 and M-1.
+	// Insert: put at front of ith chain(if not already there).
+	// Search: need to search only ith chain.
+	
+	// key - S E A R C H E X A M P L E
+	// hash- 2 0 0 4 4 4 0 2 0 4 3 3 0
+	// value 0 1 2 3 4 5 6 7 8 9 101112
+	// st[] = [ A8-E12, null, X7-S0, L11-P10, M9-H5-C4-R3], array of linked list
+	
+	// N data, M hash table -> Average length of linked list = N/M
+	// get - compute hash, find key by traversing linked list
+	// put - compute hash, if there already exist in linked list, change value, and if not, create node.
+	
+	// Under uniform hashing assumption,
+	// prob. that the number of keys in a list is within a constant factor of N/M is extremely close to 1.
+	// Number of probes(equals() and hashCode()) for search/insert is proportional to N/M.
+	// M too large - too many empty chains
+	// M too small - chains too long
+	// Typical choice: M ~ N/5 - constant-time ops.
+	// Red-Black tree - search/insert/delete take log times.
+}
