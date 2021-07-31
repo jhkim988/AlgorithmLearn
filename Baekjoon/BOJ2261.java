d89faad56b4f9dbeeb1bde1da2318501640ebffc
jhkim988 <fccva3910@gmail.com>
2021-07-31 21:08:51 +0900

    Algorithm Practice

--- Baekjoon/BOJ2261.java
+++ Baekjoon/BOJ2261.java
@@ -2,6 +2,43 @@ import java.io.*;
 import java.util.*;
 
 public class BOJ2261 {
+  static Comparator<Point2D> xSort = new Comparator<Point2D>(){
+    @Override
+    public int compare(Point2D o1, Point2D o2) {
+      if (o1.x > o2.x) {
+        return 1;
+      } else if (o1.x < o2.x) {
+        return -1;
+      } else {
+        if (o1.y > o2.y) {
+          return 1;
+        } else if (o1.y < o2.y) {
+          return -1;
+        } else {
+          return 0;
+        }
+      }
+    }      
+  };
+  static Comparator<Point2D> ySort = new Comparator<Point2D>(){
+    @Override
+    public int compare(Point2D o1, Point2D o2) {
+      if (o1.y > o2.y) {
+        return 1;
+      } else if (o1.y < o2.y) {
+        return -1;
+      } else {
+        if (o1.x > o2.x) {
+          return 1;
+        } else if (o1.x < o2.x) {
+          return -1;
+        } else {
+          return 0;
+        }
+      }
+    }
+  };
+  
   private static class Point2D {
     int x;
     int y;
@@ -9,35 +46,70 @@ public class BOJ2261 {
       this.x = x;
       this.y = y;
     }
+    int distSqaure(Point2D other) {
+      return (this.x - other.x) * (this.x - other.x) + (this.y - other.y) * (this.y - other.y);
+    }
   }
-  static long minDist(Point2D[] data) {
-    // [-10001, 10001]
-    return recur(data, -10001, 10001, -10001, 10001, 0, true);
+  static int minDist(Point2D[] data) {
+    // 1. Sort data by x-coordinate
+    Arrays.sort(data, xSort);
+    return recur(data, 0, data.length - 1);
   }
-  static long recur(Point2D[] data, int xleft, int xright, int ydown, int yup, int idx, boolean isVertical) {
-    long min1 = -1;
-    long min2 = -1;
-    if (isVertical) {
-      min1 = recur(data, xleft, data[idx].x, ydown, yup, idx + 1, !isVertical);
-      min2 = recur(data, data[idx].x, xright, ydown, yup, idx + 1, !isVertical);
-    } else {
-      min2 = recur(data, xleft, xright, ydown, data[idx].y, idx + 1, !isVertical);
-      min2 = recur(data, xleft, xright, data[idx].y, yup, idx + 1, !isVertical);
+  static int recur(Point2D[] data, int start, int end) {
+    // Partitioned Area has only two or three points.
+    if (end - start == 1) {
+      // has two points
+      return data[end].distSqaure(data[start]);
     }
 
-    long min = min1 < min2 ? min1 : min2;
+    if (end - start == 2) {
+      int min = Integer.MAX_VALUE;
+      for (int i = 0; i < 2; i++) {
+        for (int j = i + 1; j <= 2; j++) {
+          min = Math.min(min, data[i].distSqaure(data[j]));
+        }
+      }
+      return min;
+    }
+    // 2. Partition by center of points 
+    int cntIdx = (start + end) / 2;
+    int min1 = recur(data, start, cntIdx);
+    int min2 = recur(data, cntIdx + 1, end);
+    int min = Math.min(min1, min2);
+
+    // 3. In [cntIdx - min, cntIdx + min], Sort data by y-coordinate
+    ArrayList<Point2D> list = new ArrayList<>();
+    for (int i = start; i <= end; i++) {
+      int dist = data[i].x - data[cntIdx].x;
+      if (dist * dist < min) {
+        list.add(data[i]);
+      }
+    }
+    Collections.sort(list, ySort);
+    
+    // 4. Find minimum distance in [cntIdx - min, cntIdx + min]
+    for (int i = 0; i < list.size() - 1; i++) {
+      for (int j = i + 1; j < list.size(); j++) {
+        int ydist = list.get(j).y - list.get(i).y;
+        if (ydist * ydist < min) {
+          min = Math.min(list.get(i).distSqaure(list.get(j)), min);
+        } else {
+          break;
+        }
+      }
+    }
     return min;
   }
   public static void main(String[] ags) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    int N = Integer.parseInt(br.readLine());
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int N = Integer.parseInt(st.nextToken());
     Point2D[] data = new Point2D[N];
-    String[] tmp;
     for (int i = 0; i < N; i++) {
-      tmp = br.readLine().split(" ");
-      data[i] = new Point2D(Integer.parseInt(tmp[0]), Integer.parseInt(tmp[1]));
+      st = new StringTokenizer(br.readLine());
+      data[i] = new Point2D(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
     }
     bw.write(minDist(data) + "\n");
     bw.flush();
