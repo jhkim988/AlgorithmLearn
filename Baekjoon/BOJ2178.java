270910fc65d0048456feae99bd81e991b15b9500
jhKim988 <fccva3910@gmail.com>
2021-08-02 16:23:24 +0900

    Algorithm Practice

--- Baekjoon/BOJ2178.java
+++ Baekjoon/BOJ2178.java
@@ -8,6 +8,36 @@ public class BOJ2178 {
   static int[] xDi = {-1, 0, 1, 0};
   static int[] yDi = {0, -1, 0, 1};
 
+  private static class Tuple {
+    int x;
+    int y;
+    Tuple(int x, int y) {
+      this.x = x;
+      this.y = y;
+    }
+    @Override
+    public boolean equals(Object obj) {
+      if (this == obj) {
+        return true;
+      }
+      if (getClass() != obj.getClass()) {
+        return false;
+      }
+      Tuple other = (Tuple) obj;
+      if (this.x == other.x && this.y == other.y) {
+        return true;
+      }
+      return false;
+    }
+  
+    @Override
+    public int hashCode() {
+      int result = (int) x ^ (x >>> 32);
+      result = 31 * result + (int) y ^ (y >>> 32);
+      return result;
+    }
+  }
+
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -28,27 +58,35 @@ public class BOJ2178 {
   }
   static int bfs(int[][] maze, int xIdx, int yIdx, boolean[][] marked) {
     int count = 1;
-    Queue<int[]> que = new LinkedList<int[]>(); // [xIdx, yIdx]
-    
-    que.add(new int[] {xIdx, yIdx});
+    Queue<Tuple> que = new LinkedList<Tuple>(); // [xIdx, yIdx]
+    HashMap<Tuple, Tuple> edgeTo = new HashMap<>();
+    que.add(new Tuple(xIdx, yIdx));
     marked[xIdx][yIdx] = true;
 
     while (!que.isEmpty()) {
-      int[] node = que.poll();
-      if (node[0] == row - 1 && node[1] == col - 1) {
-        return count;
+      Tuple node = que.poll();
+      if (node.x == row - 1 && node.y == col - 1) {
+        break;
       }
       for (int i = 0; i < 4; i++) {
-        if (node[0] + xDi[i] < row && node[0] + xDi[i] >= 0 && node[1] + yDi[i] < col && node[1] + yDi[i] >= 0) {
-          if (maze[node[0] + xDi[i]][node[1] + yDi[i]] == 1 && !marked[node[0] + xDi[i]][node[1] + yDi[i]]) {
-            marked[node[0] + xDi[i]][node[1] + yDi[i]] = true;
-            que.add(new int[] {node[0] + xDi[i], node[1] + yDi[i]});
-            count++;
+        Tuple adj = new Tuple(node.x + xDi[i], node.y + yDi[i]);
+        if (adj.x < row && adj.x >= 0 && adj.y < col && adj.y >= 0) {
+          if (maze[adj.x][adj.y] == 1 && !marked[adj.x][adj.y]) {
+            marked[adj.x][adj.y] = true;
+            que.add(adj);
+            edgeTo.put(adj, node); 
           }
         }
       }
     }
 
-    return -1; // Error
+    Tuple start = new Tuple(0, 0);
+    Tuple goal = new Tuple(row - 1, col - 1);
+    Tuple tmp = edgeTo.get(goal);
+    while (!tmp.equals(start)) {
+      tmp = edgeTo.get(tmp);
+      count++;
+    }
+    return count + 1; // Error
   }
 }
