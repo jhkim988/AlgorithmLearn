6a07f27acccd0de26089f31594b5e29146eee4d1
jhKim988 <fccva3910@gmail.com>
2021-08-24 15:01:21 +0900

    Algorithm Practice

--- Baekjoon/BOJ13460.java
+++ Baekjoon/BOJ13460.java
@@ -9,60 +9,154 @@ public class BOJ13460 {
       this.row = row;
       this.col = col;
     }
+    @Override
+    public boolean equals(Object p) {
+      if (this == p) {
+        return true;
+      }
+      if (this.getClass() != p.getClass()) {
+        return false;
+      }
+      Pair other = (Pair) p;
+      return this.row == other.row && this.col == other.col;
+    }
+    @Override
+    public String toString() {
+      return "<" + row + ", " + col + ">";
+    }
   }
   private static class Simulation {
-    static int[] xDi = {-1, 0, 1, 0};
-    static int[] yDi = {0, -1, 0, 1};
+    static int[] rowDi = {-1, 0, 1, 0};
+    static int[] colDi = {0, -1, 0, 1};
 
     char[][] stat;
+    int row;
+    int col;
     Pair red;
     Pair blue;
     Pair goal;
     boolean redInHole;
     boolean blueInHole;
-    Simulation (char[][] stat, Pair red, Pair blue, Pair goal) {
+    boolean holeFlag;
+
+    Simulation (char[][] stat) {
       this.stat = stat;
-      this.red = red;
-      this.blue = blue;
-      this.goal = goal;
-      redInHole = false;
-      blueInHole = false;
-    }
-    void move(int direct) {
-      int redNextRow = red.row + xDi[direct];
-      int redNextCol = red.col + yDi[direct];
-      
-    }
-    void moveLeft() {
-      while (this.red.col > 0) {
-        if (stat[this.red.row][this.red.col - 1] == '#') {
-          break;
-        } else if (stat[this.red.row][this.red.col - 1] == 'B') {
-          if (this.blue.col > 0 && stat[this.blue.row][this.blue.col - 1] == '#') {
-            break;
-          } else {
-            while (this.blue.col > 0) {
-              if (stat[this.blue.row][this.blue.col - 1] == '#') {
-                break;
-              } else if (stat[this.blue.row][this.blue.col - 1] == 'O') {
-                stat[this.blue.row][this.blue.col] = '.';
-                blueInHole = true;
-              } else { // empty space
-                stat[this.blue.row][this.blue.col] = '.';
-                stat[this.blue.row][this.blue.col - 1] = 'B';
-                this.blue.col--;
-              }
-            }
+      row = this.stat.length;
+      col = this.stat[0].length;
+      for (int i = 0; i < row; i++) {
+        for(int j = 0; j < col; j++) {
+          if (this.stat[i][j] == 'R') {
+            red = new Pair(i, j);
+            this.stat[i][j] = '.';
+          } 
+          if (this.stat[i][j] == 'B') {
+            blue = new Pair(i, j);
+            this.stat[i][j] = '.';
           }
-        } else if (stat[this.red.row][this.red.col - 1] == 'O') {
-          stat[this.red.row][this.red.col] = '.';
-          redInHole = true;
-        } else { // empty space
-          stat[this.red.row][this.red.col] = '.';
-          stat[this.red.row][this.red.col - 1] = 'R';
-          this.red.col--;
+          if (this.stat[i][j] == 'O') {
+            goal = new Pair(i, j);
+          }
         }
       }
+      redInHole = false;
+      blueInHole = false;
+      holeFlag = false;
+    }
+    ArrayList<Pair> move(int direct, Pair crntRed, Pair crntBlue) {
+      Pair redMove = moveTmp(direct, crntRed);
+      if (holeFlag) {
+        if (redMove.equals(goal)) {
+          redInHole = true;
+        }
+        holeFlag = false;
+      }
+      Pair blueMove = moveTmp(direct, crntBlue);
+      if (holeFlag) {
+        if (blueMove.equals(goal)) {
+          blueInHole = true;
+        }
+        holeFlag = false;
+      } 
+      if (redMove.row == blueMove.row && redMove.col == blueMove.col) {
+        if (crntRed.row == crntBlue.row) { // horizontal
+          if (crntRed.col < crntBlue.col) { // Red - Blue
+            if (direct < 2) { // minus direction
+              blueMove.col++;
+            } else { // plus direction
+              redMove.col--;
+            }
+          } else { // Blue - Red
+            if (direct < 2) { // minus direction
+              redMove.col++;
+            } else {
+              blueMove.col--;
+            }
+          }
+        } else { // vertical
+          if (crntRed.row < crntBlue.row) { // Red(up) - Blue(down)
+            if (direct < 2) { // minus direction
+              blueMove.row++;
+            } else { // plus direction
+              redMove.row--;
+            }
+          } else { // Blue(up) - Red(down)
+            if (direct < 2) { // minus direction
+              redMove.row++;
+            } else {
+              blueMove.row--;
+            }
+          }
+        }
+      } 
+      ArrayList<Pair> result = new ArrayList<>();
+      result.add(redMove);
+      result.add(blueMove);
+      return result;
+    }
+    Pair moveTmp(int direct, Pair pair) {
+      int nextRow = pair.row + rowDi[direct];
+      int nextCol = pair.col + colDi[direct];
+      while (nextRow >= 0 && nextRow < row && nextCol >= 0 && nextCol < col && stat[nextRow][nextCol] != '#') {
+        if (stat[nextRow][nextCol] == 'O') {
+          holeFlag = true;
+          return goal;
+        } 
+        nextRow += rowDi[direct];
+        nextCol += colDi[direct];
+      }
+      return new Pair(nextRow - rowDi[direct], nextCol - colDi[direct]);
+    }
+    public String print(Pair rred, Pair bblue) {
+      StringBuilder sb = new StringBuilder();
+      for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+          if (rred.row == i && rred.col == j) {
+            sb.append('R');
+          } else if (bblue.row == i && bblue.col == j) {
+            sb.append('B');
+          } else {
+            sb.append(stat[i][j]);
+          }
+        }
+        sb.append('\n');
+      }
+      return sb.toString();
+    }
+  }
+  private static class Action {
+    int crnt;
+    int count;
+    Pair red;
+    Pair blue;
+    Action(int crnt, int count, Pair red, Pair blue) {
+      this.crnt = crnt;
+      this.count = count;
+      this.red = new Pair(red.row, red.col);
+      this.blue = new Pair(blue.row, blue.col);
+    }
+    @Override
+    public String toString() {
+      return "count: " + count + "/Move:" + crnt;
     }
   }
   public static void main(String[] args) throws IOException {
@@ -75,6 +169,55 @@ public class BOJ13460 {
     for (int i = 0; i < row; i++) {
       stat[i] = br.readLine().toCharArray();
     }
+    Simulation simul = new Simulation(stat);
+    // Pair initRed = new Pair(4, 3);
+    // Pair initBlue = new Pair(4, 2);
+    // ArrayList<Pair> redblue = simul.move(3, initRed, initBlue);
+    // System.out.println(simul.print(redblue.get(0), redblue.get(1)));
+    // redblue = simul.move(0, redblue.get(0), redblue.get(1));
+    // System.out.println(simul.print(redblue.get(0), redblue.get(1)));
 
+    Queue<Action> que = new LinkedList<>();
+    Pair initRed = new Pair(simul.red.row, simul.red.col);
+    Pair initBlue = new Pair(simul.blue.row, simul.blue.col);
+    for (int i = 0; i < 4; i++) {
+      que.add(new Action(i, 1, initRed, initBlue));
+    }
+    while (!que.isEmpty()) {
+      Action action = que.poll();
+      if (action.count > 10) {
+        break;
+      }
+      ArrayList<Pair> redblue = simul.move(action.crnt, action.red, action.blue);
+      // System.out.println("count: " + action.count + " / move: " + action.crnt);
+      // System.out.println("red: " + redblue.get(0) + " / blue: " + redblue.get(1));
+      // System.out.println(simul.print(action.red, action.blue));
+      // System.out.println(simul.print(redblue.get(0), redblue.get(1)));
+      if (simul.redInHole) {
+        if (simul.blueInHole) {
+          simul.redInHole = false;
+          simul.blueInHole = false;
+          continue;
+        }
+        // System.out.println("Find");
+        bw.write(action.count + "\n");
+        bw.flush();
+        return;
+      }
+      if (simul.blueInHole) {
+        simul.blueInHole = false;
+        if (simul.redInHole) {
+          simul.redInHole = false;
+        }
+        continue;
+      }
+      for (int i = 0; i < 4; i++) {
+        if (action.crnt % 2 == i % 2) continue;
+         que.add(new Action(i, action.count + 1, redblue.get(0), redblue.get(1)));
+      }
+    }
+    // System.out.println("Here");
+    bw.write("-1\n");
+    bw.flush();
   }
 }
