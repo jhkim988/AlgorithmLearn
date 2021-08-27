93df8813d194337bd43ea0b5c36fa5e1b86245ab
jhKim988 <fccva3910@gmail.com>
2021-08-27 11:55:25 +0900

    Algorithm Practice

---
+++ Baekjoon/BOJ1086.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ1086 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    int N = Integer.parseInt(br.readLine());
+    
+  }
+}
