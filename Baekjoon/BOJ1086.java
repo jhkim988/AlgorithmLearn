64e3422d5c512ff9605764bad4bbe09881036786
jhKim988 <fccva3910@gmail.com>
2021-08-30 15:58:18 +0900

    Algorithm Practice

--- Baekjoon/BOJ1086.java
+++ Baekjoon/BOJ1086.java
@@ -6,6 +6,21 @@ public class BOJ1086 {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     int N = Integer.parseInt(br.readLine());
-    
+    String[] data = new String[N];
+    for (int i = 0; i < N; i++) {
+      data[i] = br.readLine();
+    }
+    int K = Integer.parseInt(br.readLine());
+
+    long[][] dp = new long[N + 1][1 << N];
+    // for (int i = 0; i < N; i++) {
+    //   dp[1][1 << i] = data[i] % K;
+    // }
+
+    // for (int i = 2; i <= N; i++) {
+    //   for (int j = 0; j < N; j++) {
+    //     dp[i][bit | 1 << j] = ((dp[i - 1][bit] * pow(10, data[j].length())) % K + dp[1][1 << j]) % K;
+    //   }
+    // }
   }
 }
