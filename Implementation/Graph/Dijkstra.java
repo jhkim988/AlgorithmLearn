3c14fb97d8c5ff3d02bd1aeb293f68daf1b02b65
jhkim988 <fccva3910@gmail.com>
2021-09-12 23:53:42 +0900

    Algorithm Implementation Summarize

---
+++ Implementation/Graph/Dijkstra.java
@@ -0,0 +1,69 @@
+package Graph;
+
+import java.util.*;
+
+public class Dijkstra {
+  private static class Node implements Comparable<Node> {
+    int end;
+    int weight;
+    Node(int end, int weight) {
+      this.end = end;
+      this.weight = weight;
+    }
+    @Override
+    public int compareTo(Node other) {
+      return this.weight - other.weight;
+    }
+  }
+  public static void main(String[] args) {
+    // Graph:
+    final int V = 20; // number of vertex, id: 1 ~ 20
+    final int E = 10; // number of edge
+    final int weightLim = 10;
+    ArrayList<Queue<Node>> graph = new ArrayList<>(); // ArrayList of Iterable Object
+    
+    // Graph Initialize:
+    for (int i = 0; i <= V; i++) {
+      graph.add(new LinkedList<>());
+    }
+    for (int i = 0; i < E; i++) { 
+      // Generate Random Edge
+      int start = (int) (Math.random() * 20) + 1;
+      int end = (int) (Math.random() * 20) + 1;
+      int weight = (int) (Math.random() * weightLim);
+      graph.get(start).add(new Node(end, weight));
+      // graph.get(end).add(new Node(start, weight)); // Undirected Graph
+    }
+
+    // Dijkstra Algorithm:
+    // Use cost array greedly.
+    int start = 1;
+    int target = 20;
+    Node startNode = new Node(start, 0);
+    int[] cost = new int[V + 1];
+
+    // cost array initialize:
+    for (int i = 1; i <= V; i++) {
+      cost[i] = Integer.MAX_VALUE;
+    }
+
+    PriorityQueue<Node> pq = new PriorityQueue<>();
+    pq.add(startNode);
+    while (!pq.isEmpty()) {
+      Node crnt = pq.poll();
+
+      if (crnt.end == target) {
+        // Find. cost: cost[target]
+        break;
+      }
+
+      for (Node adj : graph.get(crnt.end)) {
+        // If we find a shorter way to cost[adj.end], Update cost[adj.end]
+        if (cost[adj.end] <= cost[crnt.end] + adj.weight) continue;
+        pq.add(new Node(adj.end, cost[crnt.end] + adj.weight));
+        cost[adj.end] = cost[crnt.end] + adj.weight;
+      }
+    }
+    // Not Found. cost: Integer.MAX_VALUE
+  }
+}
---
+++ Implementation/Graph/ZeroOneBFS.java
@@ -0,0 +1,62 @@
+package Graph;
+
+import java.util.*;
+
+public class ZeroOneBFS {
+  private static class Node {
+    int end;
+    int weight;
+    Node(int end, int weight) {
+      this.end = end;
+      this.weight = weight;
+    }
+  }
+  public static void main(String[] args) {
+    // Graph:
+    final int V = 20; // number of vertex, id: 1 ~ 20
+    final int E = 10; // number of edge
+    ArrayList<Queue<Node>> graph = new ArrayList<>(); // ArrayList of Iterable Object
+    
+    // Graph Initialize:
+    for (int i = 0; i <= V; i++) {
+      graph.add(new LinkedList<>());
+    }
+    for (int i = 0; i < E; i++) { 
+      // Generate Random Edge
+      int start = (int) (Math.random() * 20) + 1;
+      int end = (int) (Math.random() * 20) + 1;
+      int weight = (int) (Math.random() * 2);
+      graph.get(start).add(new Node(end, weight));
+      // graph.get(end).add(new Node(start, weight)); // Undirected Graph
+    }
+
+    // 0-1 BFS:
+    // Use Deque.
+    int start = 1;
+    int target = 20;
+    Node startNode = new Node(start, 0);
+    Deque<Node> deq = new LinkedList<>();
+    boolean[] marked = new boolean[V + 1];
+    deq.addFirst(startNode);
+    while (!deq.isEmpty()) {
+      Node crnt = deq.removeFirst();
+      marked[crnt.end] = true;
+
+      if (crnt.end == target) {
+        // Find. cost: crnt.weight;
+        break;
+      }
+
+      for (Node adj : graph.get(crnt.end)) {
+        // Zero cost: addFirst / One cost: addLast
+        if (marked[adj.end]) continue;
+        if (adj.weight == 0) {
+          deq.addFirst(new Node(adj.end, crnt.weight));
+        } else {
+          deq.addLast(new Node(adj.end, crnt.weight + 1));
+        }
+      }
+    }
+    // Not Found: disconnected
+  }
+}
