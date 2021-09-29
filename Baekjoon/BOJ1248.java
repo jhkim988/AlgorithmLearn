03ab3733d47928ca46a116837eaa9d17601d964b
jhkim988 <fccva3910@gmail.com>
2021-09-29 18:59:09 +0900

    Algorithm Practice

--- Baekjoon/BOJ1248.java
+++ Baekjoon/BOJ1248.java
@@ -5,11 +5,22 @@ public class BOJ1248 {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     int N = Integer.parseInt(br.readLine());
-    char[] sign = br.readLine().toCharArray();
+    char[][] sign = new char[N][N];
+    String input = br.readLine();
+    int ptr = 0;
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < i; j++) {
+        sign[i][j] = 'x'; // dummy
+      }
+      for (int j = i; j < N; j++) {
+        sign[i][j] = input.charAt(ptr);
+        ptr++;
+      }
+    }
     bw.write(answer(N, sign));
     bw.flush();
   }
-  static String answer(int N, char[] sign) {
+  static String answer(int N, char[][] sign) {
     int[] result = new int[N];
     boolean[] flag = {false};
     recur(0, N, sign, result, flag);
@@ -20,42 +31,45 @@ public class BOJ1248 {
     sb.append('\n');
     return sb.toString();
   }
-  static void recur(int depth, int N, char[] sign, int[] result, boolean[] flag) {
+  static void recur(int depth, int N, char[][] sign, int[] result, boolean[] flag) {
     if (depth == N) {
       flag[0] = true;
       return;
     }
-    int checkIdx = depth * N - (depth - 1);
-    if (sign[checkIdx] == '+') {
+    if (sign[depth][depth] == '+') {
       recur: for (int i = 1; i <= 10; i++) {
         result[depth] = i;
         int sum = 0;
+        int ptr = depth;
         for (int j = depth; j >= 0; j--) {
           sum += result[j];
-          if (sign[checkIdx - (N - depth - j - 1)] == '+') {
+          if (sign[ptr][depth] == '+') {
             if (sum <= 0) continue recur;
-          } else if (sign[checkIdx - (N - depth - j)] == '-') {
+          } else if (sign[ptr][depth] == '-') {
             if (sum >= 0) continue recur;
           } else {
             if (sum != 0) continue recur;
           }
+          ptr--;
         }
         recur(depth + 1, N, sign, result, flag);
         if (flag[0]) return;
       }
-    } else if (sign[checkIdx] == '-') {
+    } else if (sign[depth][depth] == '-') {
       recur: for (int i = -1; i >= -10; i--) {
         result[depth] = i;
         int sum = 0;
+        int ptr = depth;
         for (int j = depth; j >= 0; j--) {
           sum += result[j];
-          if (sign[checkIdx - (N - depth - j - 1)] == '+') {
+          if (sign[ptr][depth] == '+') {
             if (sum <= 0) continue recur;
-          } else if (sign[checkIdx - (N - depth - j)] == '-') {
+          } else if (sign[ptr][depth] == '-') {
             if (sum >= 0) continue recur;
           } else {
             if (sum != 0) continue recur;
           }
+          ptr--;
         }
         recur(depth + 1, N, sign, result, flag);
         if (flag[0]) return;
@@ -63,18 +77,20 @@ public class BOJ1248 {
     } else {
       result[depth] = 0;
       int sum = 0;
+      int ptr = depth;
       for (int j = depth; j >= 0; j--) {
         sum += result[j];
-        if (sign[checkIdx - (N - depth - j - 1)] == '+') {
+        if (sign[ptr][depth] == '+') {
           if (sum <= 0) return;
-        } else if (sign[checkIdx - (N - depth - j - 1)] == '-') {
+        } else if (sign[ptr][depth] == '-') {
           if (sum >= 0) return;
         } else {
           if (sum != 0) return;
         }
-        recur(depth + 1, N, sign, result, flag);
-        if (flag[0]) return;
+        ptr--;
+      }
+      recur(depth + 1, N, sign, result, flag);
+      if (flag[0]) return;
       }
     }
-  }
 }
