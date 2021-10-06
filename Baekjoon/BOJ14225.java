a4c094632a35dc8468a28d0ebea241b558ea9cc3
jhKim988 <fccva3910@gmail.com>
2021-10-06 09:06:33 +0900

    Algorithm Practice

--- Baekjoon/BOJ14225.java
+++ Baekjoon/BOJ14225.java
@@ -21,5 +21,35 @@ public class BOJ14225 {
     for (int el : data) {
       sum += el; // sum <= 20_000_000;
     }
+    boolean[] check = new boolean[sum + 1];
+    // boolean[] marked = new boolean[data.length];
+    recur(0, 0, data, check);
+    for (int i = 1; i <= sum; i++) {
+      if (check[i] == false) return i;
+    }
+    return sum + 1;
+  }
+  static void timeLimitExceed(int depth, int sum, int[] data, boolean[] marked, boolean[] check) {
+    // 순서까지 고려된 재귀, 순서는 필요 없다.
+    if (depth >= data.length) {
+      return;
+    }
+    
+    for (int i = depth; i < data.length; i++) {
+      if (marked[i]) continue;
+      marked[i] = true;
+      check[sum + data[i]] = true;
+      recur(depth + 1, sum + data[i], data, marked, check);
+      marked[i] = false;
+    }
+  }
+
+  static void recur(int depth, int sum, int[] data, boolean[] check) {
+    if (depth >= data.length) {
+      return;
+    }
+    recur(depth + 1, sum, data, check);
+    check[sum + data[depth]] = true;
+    recur(depth + 1, sum + data[depth], data, check);
   }
 }
