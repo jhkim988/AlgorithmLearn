058ebc655ba3152a057b9415f5dcfa02a52cb4b8
jhKim988 <fccva3910@gmail.com>
2021-10-13 12:27:34 +0900

    Algorithm Practice

--- Baekjoon/BOJ1080.java
+++ Baekjoon/BOJ1080.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class BOJ59270 {
+public class BOJ1080 {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
