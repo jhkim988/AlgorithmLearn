75ebc17828e2e8148475209aaa070a60bc00627e
jhKim988 <fccva3910@gmail.com>
2021-10-14 15:46:52 +0900

    Algorithm Practice

--- Baekjoon/BOJ6087.java
+++ Baekjoon/BOJ6087.java
@@ -91,29 +91,16 @@ public class BOJ6087 {
     return min;
   }
   static int ZeroOneBFS() {
-    boolean[][][] marked = new boolean[row][col][4];
+    int[][] marked = new int[row][col];
     Deque<Pair> deq = new LinkedList<>();
     deq.addLast(target[0]);
-    for (int i = 0; i < 4; i++) {
-      marked[target[0].row][target[0].col][i] = true;
-    }
-    char[][] print = new char[row][col];
     for (int i = 0; i < row; i++) {
-      Arrays.fill(print[i], '_');
+      Arrays.fill(marked[i], Integer.MAX_VALUE);
     }
-    int ptr = 0;
+    marked[target[0].row][target[0].col] = 0;
     while (!deq.isEmpty()) {
       Pair crnt = deq.removeFirst();
-      if (crnt.numReflect > ptr) {
-        for (int i = 0; i < row; i++) {
-          System.out.println(print[i]);
-        }
-        System.out.println("----------");
-        ptr++;
-      }
-      print[crnt.row][crnt.col] = (char) Math.min(print[crnt.row][crnt.col], (crnt.numReflect + '0'));
       if (crnt.row == target[1].row && crnt.col == target[1].col) {
-
         return crnt.numReflect;
       }
       for (int i = 0; i < 4; i++) {
@@ -121,32 +108,16 @@ public class BOJ6087 {
         int nextCol = crnt.col + colDi[i];
         if (nextRow < 0 || nextRow >= row || nextCol < 0 || nextCol >= col) continue;
         if (map[nextRow][nextCol] == '*') continue;
-        if (marked[nextRow][nextCol][i]) continue;
+        if (marked[nextRow][nextCol] < crnt.numReflect) continue;
         if (crnt.direction == -1 || crnt.direction == i) {
           deq.addFirst(new Pair(nextRow, nextCol, crnt.numReflect, i));
         } else {
           deq.addLast(new Pair(nextRow, nextCol, crnt.numReflect + 1, i));
         }
-        marked[nextRow][nextCol][i] = true;
+        marked[nextRow][nextCol] = crnt.numReflect;
       }
     }
     // No Case
     return -1;
-  }
-  static void print(int[][][] numReflect) {
-    for (int i = 0; i < row; i++) {
-      for (int j = 0; j < col; j++) {
-        int tmp = numReflect[i][j][0];
-        for (int k = 0; k < 4; k++) {
-          tmp = Math.min(tmp, numReflect[i][j][k]);
-        }
-        if (tmp == Integer.MAX_VALUE) {
-          System.out.print('*');
-        } else {
-          System.out.print(tmp);
-        }
-      }
-      System.out.println();
-    }
   }
 }
