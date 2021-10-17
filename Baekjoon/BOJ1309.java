05c66a8e0b43a092fd9459e2c602845d4ad32954
jhkim988 <fccva3910@gmail.com>
2021-10-17 21:57:56 +0900

    Algorithm Practice

--- Baekjoon/BOJ1309.java
+++ Baekjoon/BOJ1309.java
@@ -2,6 +2,7 @@ import java.io.*;
 
 public class BOJ1309 {
   public static void main(String[] args) throws IOException {
+    final int remainder = 9901;
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     int N = Integer.parseInt(br.readLine());
@@ -9,17 +10,15 @@ public class BOJ1309 {
     dp[1][0] = dp[1][1] = dp[1][2] = 1;
     for (int i = 2; i <= N; i++) {
       for (int j = 0; j < 3; j++) {
-        dp[i][0] += dp[i - 1][j];
+        dp[i][0] = (dp[i][0] + dp[i - 1][j]) % remainder;
       }
-      dp[i][1] = dp[i - 1][0] + dp[i - 1][2];
-      dp[i][2] = dp[i - 1][0] + dp[i - 1][1];
-
-      for (int j = 0; j < 3; j++) {
-        dp[i][j] %= 9901;
-      }
+      dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % remainder;
+      dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % remainder;
     }
-    int sum = dp[N][0]  + dp[N][1] + dp[N][2];
-    sum %= 9901;
+    int sum = 0;
+    for (int i = 0; i < 3; i++) {
+      sum = (sum + dp[N][i]) % remainder;
+    }
     bw.write(sum + "\n");
     bw.flush();
   }
