da16f9594a24f221348d039bce87073235e367a0
jhkim988 <fccva3910@gmail.com>
2021-10-17 21:48:47 +0900

    Algorithm Practice

---
+++ Baekjoon/BOJ2914.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class BOJ2914 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int A = Integer.parseInt(st.nextToken());
+    int I = Integer.parseInt(st.nextToken());
+    bw.write((A * (I - 1) + 1) + "\n");
+    bw.flush();
+  }
+}
