77652f10527aa991d5ea60371454a2af187bc33e
jhkim988 <fccva3910@gmail.com>
2021-10-25 16:52:41 +0900

    Algorithm Practice

---
+++ Baekjoon/BOJ5338.java
@@ -0,0 +1,5 @@
+public class BOJ5338 {
+  public static void main(String[] args) {
+    System.out.println("       _.-;;-._\n'-..-'|   ||   |\n'-..-'|_.-;;-._|\n'-..-'|   ||   |\n'-..-'|_.-''-._|");
+  }
+}
