f16007ce08580de13899b597de630e85d85bcdaa
jhKim988 <fccva3910@gmail.com>
2021-11-17 13:20:12 +0900

    Algorithm Practice

--- Baekjoon/BOJ16935.java
+++ Baekjoon/BOJ16935.java
@@ -2,18 +2,16 @@ import java.io.*;
 import java.util.*;
 
 public class BOJ16935 {
-  static int N;
-  static int M;
-  static int[] rowDi = {0, 0, N/2, N/2, 0};
-  static int[] colDi = {0, M/2, M/2, 0, 0};
+  static int[] rowDi;
+  static int[] colDi;
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     StringTokenizer st = new StringTokenizer(br.readLine());
-    N = Integer.parseInt(st.nextToken());
-    M = Integer.parseInt(st.nextToken());
+    int N = Integer.parseInt(st.nextToken());
+    int M = Integer.parseInt(st.nextToken());    
     int numOper = Integer.parseInt(st.nextToken());
-
+  
     int[][] origin = new int[N][M];
     for (int i = 0; i < N; i++) {
       st = new StringTokenizer(br.readLine());
@@ -52,19 +50,25 @@ public class BOJ16935 {
         }
     }
 
+    bw.write(print(origin));
+    bw.flush();
+  }
+  static String print(int[][] origin) {
+    int N = origin.length;
+    int M = origin[0].length;
     StringBuilder sb = new StringBuilder();
-    for (int i = 0; i < origin.length; i++) {
+    for (int i = 0; i < N; i++) {
       sb.append(origin[i][0]);
-      for (int j = 1; j < origin[0].length; j++) {
+      for (int j = 1; j < M; j++) {
         sb.append(' ').append(origin[i][j]);
       }
       sb.append('\n');
     }
-
-    bw.write(sb.toString());
-    bw.flush();
+    return sb.toString();
   }
   static void upDownInversion(int[][] origin) {
+    int N = origin.length;
+    int M = origin[0].length;
     for (int col = 0; col < M; col++) {
       for (int row = 0; row < N/2; row++) {
         int tmp = origin[row][col];
@@ -74,6 +78,8 @@ public class BOJ16935 {
     }
   }
   static void leftRightInversion(int[][] origin) {
+    int N = origin.length;
+    int M = origin[0].length;
     for (int row = 0; row < N; row++) {
       for (int col = 0; col < M/2; col++) {
         int tmp = origin[row][col];
@@ -82,14 +88,9 @@ public class BOJ16935 {
       }
     }
   }
-  static int[][] copy(int[][] origin) {
-    int[][] copy = new int[N][M];
-    for (int i = 0; i < N; i++) {
-      System.arraycopy(origin[i], 0, copy[i], 0, M);
-    }
-    return copy;
-  }
   static int[][] rightRotation(int[][] origin) {
+    int N = origin.length;
+    int M = origin[0].length;
     int[][] copy = new int[M][N];
     for (int i = 0; i < M; i++) {
       for (int j = 0; j < N; j++) {
@@ -99,6 +100,8 @@ public class BOJ16935 {
     return copy;
   }
   static int[][] leftRotation(int[][] origin) {
+    int N = origin.length;
+    int M = origin[0].length;
     int[][] copy = new int[M][N];
     for (int i = 0; i < M; i++) {
       for (int j = 0; j < N; j++) {
@@ -108,25 +111,33 @@ public class BOJ16935 {
     return copy;
   }
   static int[][] groupRightRotation(int[][] origin) {
-    int[][] copy = copy(origin);
-    for (int i = 0; i < N/2; i++) {
-      for (int j = 0; j < M/2; j++) {
-        for (int k = 0; k < 4; k++) {
-          origin[i + rowDi[k + 1]][j + colDi[k + 1]] = copy[i + rowDi[k]][j + colDi[k]];
+    int N = origin.length;
+    int M = origin[0].length;
+    int[][] copy = new int[N][M];
+    rowDi = new int[] {0, 0, N/2, N/2, 0};
+    colDi = new int[] {0, M/2, M/2, 0, 0};
+    for (int k = 0; k < 4; k++) {
+      for (int i = 0; i < N/2; i++) {
+        for (int j = 0; j < M/2; j++) {
+          copy[i + rowDi[k + 1]][j + colDi[k + 1]] = origin[i + rowDi[k]][j + colDi[k]];
         }
       }
     }
-    return origin;
+    return copy;
   }
   static int[][] groupLeftRotation(int[][] origin) {
-    int[][] copy = copy(origin);
-    for (int i = 0; i < N/2; i++) {
-      for (int j = 0; j < M/2; j++) {
-        for (int k = 5; k > 0; k--) {
-          origin[i + rowDi[k]][j + colDi[k]] = copy[i + rowDi[k - 1]][j + colDi[k - 1]];
+    int N = origin.length;
+    int M = origin[0].length;
+    int[][] copy = new int[N][M];
+    rowDi = new int[] {0, 0, N/2, N/2, 0};
+    colDi = new int[] {0, M/2, M/2, 0, 0};
+    for (int k = 0; k < 4; k++) {
+      for (int i = 0; i < N/2; i++) {
+        for (int j = 0; j < M/2; j++) {
+          copy[i + rowDi[k]][j + colDi[k]] = origin[i + rowDi[k + 1]][j + colDi[k + 1]];
         }
       }
     }
-    return origin;
+    return copy;
   }
 }
