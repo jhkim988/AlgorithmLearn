bdabfa37ce22b599f1bbce44ffee01b6e799a1f5
jhkim988 <fccva3910@gmail.com>
2021-12-04 03:18:29 +0900

    Algorithm Practice

---
+++ Baekjoon/BOJ10699.java
@@ -0,0 +1,8 @@
+import java.time.*;
+
+public class BOJ10699 {
+  public static void main(String[] args) {
+    LocalDate now = LocalDate.now(ZoneOffset.ofHours(9));
+    System.out.println(now);
+  }
+}
