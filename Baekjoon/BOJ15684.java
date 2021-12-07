fa9372caeff4058d1bd8c54c8f05069b1c83396d
jhKim988 <fccva3910@gmail.com>
2021-12-07 13:17:43 +0900

    Algorithm Practice

--- Baekjoon/BOJ15684.java
+++ Baekjoon/BOJ15684.java
@@ -3,6 +3,7 @@ import java.util.*;
 
 public class BOJ15684 {
   static int min = 4;
+  static int[] origin;
   private static class Pair {
     int height;
     int line;
@@ -11,95 +12,90 @@ public class BOJ15684 {
       this.line = line;
     }
   }
-   public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     StringTokenizer st = new StringTokenizer(br.readLine());
-    int N = Integer.parseInt(st.nextToken());
-    int M = Integer.parseInt(st.nextToken());
-    int H = Integer.parseInt(st.nextToken());
+    int numLine = Integer.parseInt(st.nextToken());
+    int numSwap = Integer.parseInt(st.nextToken());
+    int height = Integer.parseInt(st.nextToken());
 
-    int[] lines = new int[N];
-    for (int i = 0; i < N; i++) {
-      lines[i] = i;
-    }
-    boolean[][] marked = new boolean[H][N - 1];
-    ArrayList<Pair> fn = new ArrayList<>();
-    for (int i = 0; i < M; i++) {
+    ArrayList<Pair> fns = new ArrayList<>();
+    int[][] marked = new int[height][numLine - 1];
+    for (int i = 0; i < numSwap; i++) {
       st = new StringTokenizer(br.readLine());
-      int height = Integer.parseInt(st.nextToken());
-      int line = Integer.parseInt(st.nextToken());
-      marked[height - 1][line - 1] = true;
-      fn.add(new Pair(height - 1, line - 1));
+      int h = Integer.parseInt(st.nextToken());
+      int l = Integer.parseInt(st.nextToken());
+      fns.add(new Pair(h - 1, l - 1));
+      marked[h - 1][l - 1] = 1;
     }
-    Collections.sort(fn, new Comparator<Pair>(){
+    Collections.sort(fns, new Comparator<Pair>(){
       @Override
-      public int compare(Pair o1,Pair o2) {
+      public int compare(Pair o1, Pair o2) {
         return o1.height == o2.height ? o1.line - o2.line : o1.height - o2.height;
-      }      
+      }
     });
-    int[] origin = new int[N];
-    for (int i = 0; i < N; i++) origin[i] = i;
-    ArrayList<int[]> front = new ArrayList<>();
-    ArrayList<int[]> inverse = new ArrayList<>();    
-    for (int i = 0; i <= M; i++) {
-      int[] tmp1 = new int[N];
-      int[] tmp2 = new int[N];
-      System.arraycopy(origin, 0, tmp1, 0, N);
-      System.arraycopy(origin, 0, tmp2, 0, N);
-      for (int j = 0; j < i; j++) {
-        int line = fn.get(j).line;
-        swap(tmp1, line, line + 1);
+    ArrayList<int[]> after = new ArrayList<>(numSwap);
+    for (int i = 0; i < numSwap; i++) after.add(origin);
+    origin = new int[numLine];
+    for (int i = 0; i < numLine; i++) origin[i] = i;
+    for (int i = numSwap - 1; i >= 0; i--) {
+      int[] tmp = new int[numLine];
+      System.arraycopy(origin, 0, tmp, 0, numLine);
+      Pair swp = fns.get(i);
+      swap(tmp, swp.line, swp.line + 1);
+      if (i == numSwap - 1) {
+        after.set(i, tmp);
+      } else {
+        after.set(i, composite(tmp, after.get(i + 1)));
       }
-      front.add(tmp1);
-      for (int j = 0; j < i; j++) {
-        int line = fn.get(i - j - 1).line;
-        swap(tmp2, line, line + 1);
-      }
-      inverse.add(tmp2);
     }
+    int[] fn = new int[numLine];
+    System.arraycopy(origin, 0, fn, 0, numLine);
+    recur(0, 0, 0, marked, fn, fns, after);
 
-    recur(0, 0, 0, origin, marked, front, inverse);
-    bw.write(min + "\n");
+    bw.write(min == 4 ? "-1\n" : (min + "\n"));
     bw.flush();
   }
-  static void swap(int[] lines, int idx1, int idx2) {
-    int tmp = lines[idx1];
-    lines[idx1] = lines[idx2];
-    lines[idx2] = tmp;
+  static void swap(int[] arr, int idx1, int idx2) {
+    int tmp = arr[idx1];
+    arr[idx1] = arr[idx2];
+    arr[idx2] = tmp;
   }
-  static int[] action(int[] before, int[] after) {
+  static int[] composite(int[] before, int[] after) {
     int[] result = new int[before.length];
-    for (int i = 0; i < result.length; i++) {
-      result[i] = before[after[i]];
-    } 
+    for (int i = 0; i < result.length; i++) result[i] = before[after[i]];
     return result;
   }
-  static int[] partial(ArrayList<int[]> front, ArrayList<int[]> inverse, int from, int to) {
-    // [from, to]
-    if (from == 0) return front.get(to);
-    return action(inverse.get(from - 1), front.get(to));
-  }
-  static void recur(int depth, int markedPtr, int fnPtr, int[] fn, boolean[][] marked, ArrayList<int[]> front, ArrayList<int[]> inverse) {
-    if (depth >= min) return;
-    int h = markedPtr / marked.length;
-    int l = markedPtr % marked.length;
-    swap(fn, l, l + 1);
-    int[] tmp = action(fn, partial(front, inverse, fnPtr, front.size() - 1));
-    boolean flag = true;
-    for (int i = 0; i < tmp.length; i++) flag = flag && tmp[i] == i;
-    if (flag) min = Math.min(min, depth);
+  static void recur(int depth, int ptr, int fnPtr, int[][] marked, int[] fn, ArrayList<Pair> fns, ArrayList<int[]> after) {
+    if (depth >= min || ptr > marked.length * marked[0].length) return;
+    // System.out.println("call depth: " + depth + ", ptr: " + ptr + ", fnPtr: " + fnPtr);
+    // for (int i = 0; i < marked.length; i++) System.out.println(Arrays.toString(marked[i]));
 
-    for (int cur = markedPtr; cur < marked.length * marked[0].length; cur++) {
-      int height = cur / marked.length;
-      int line = cur % marked.length;
-      if (marked[height][line]) {
-        fnPtr++;
+    // check:
+    boolean check = true;
+    int[] tmp = (fnPtr >= after.size()) ? fn : composite(fn, after.get(fnPtr));
+    for (int i = 0; i < fn.length; i++) check = check && tmp[i] == i;
+    if (check) min = Math.min(min, depth);
+    // if (check) System.out.println("PASS");
+
+    // loop:
+    for (int cur = ptr; cur < marked.length * marked[0].length; cur++) {
+      int line = cur % marked[0].length;
+      int height = cur / marked[0].length;
+      if ((line > 0 && marked[height][line - 1] != 0) || (line < marked[0].length - 1 && marked[height][line + 1] != 0)) continue;
+      int[] nextFn = new int[fn.length];
+      System.arraycopy(fn, 0, nextFn, 0, fn.length);
+      if (marked[height][line] == 1) {
+        Pair swp = fns.get(fnPtr++);
+        swap(nextFn, swp.line, swp.line + 1);
+        fn = nextFn;
         continue;
       }
-      if (line > 0 && marked[height][line - 1] || line < marked[0].length - 1 && marked[height][line + 1]) continue;
-      recur(depth + 1, height * marked.length + line, fnPtr, fn, marked, front, inverse);
+      marked[height][line] = 2;
+      swap(nextFn, line, line + 1);
+      recur(depth + 1, cur + 1, fnPtr, marked, nextFn, fns, after);
+      marked[height][line] = 0;
     }
-    swap(fn, l, l + 1);
   }
 }
