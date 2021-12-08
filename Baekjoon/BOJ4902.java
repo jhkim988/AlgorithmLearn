996b62dab2b917d05577922800602f301bfb9753
jhKim988 <fccva3910@gmail.com>
2021-12-08 13:21:58 +0900

    Algorithm Practice

--- Baekjoon/BOJ4902.java
+++ Baekjoon/BOJ4902.java
@@ -19,7 +19,14 @@ public class BOJ4902 {
           triangle[i][j] = Integer.parseInt(st.nextToken());
         }
       }
-
+      int[][] pSum = new int[numLine][];
+      for (int i = 0; i < numLine; i++) {
+        pSum[i] = new int[triangle[i].length];
+        pSum[i][0] = triangle[i][0];
+        for (int j = 1; j < triangle[i].length; j++) {
+          pSum[i][j] = pSum[i][j - 1] + triangle[i][j];
+        }
+      }
       int max = Integer.MIN_VALUE;
       for (int row = 0; row < numLine; row++) {
         for (int col = 0; col < triangle[row].length; col++) {
@@ -27,18 +34,20 @@ public class BOJ4902 {
           max = Math.max(sum, max);
           if (col % 2 == 0) {
             for (int size = 1; row + size < numLine; size++) {
-              for (int cur = 0; cur < 2 * size + 1; cur++) sum += triangle[row + size][col + cur];
+              // for (int cur = 0; cur < 2 * size + 1; cur++) sum += triangle[row + size][col + cur];
+              sum += col == 0 ? pSum[row + size][col + 2 * size] : pSum[row + size][col + 2 * size] - pSum[row + size][col - 1];
               max = Math.max(sum, max);
             }            
           } else { // inverted triangle
             for (int size = 1; col < triangle[row - size].length && col - (2 * size + 1) + 1 >= 0 && row >= size; size++) {
-              for (int cur = 0; cur < 2 * size + 1; cur++) sum += triangle[row - size][col - cur];
+              // for (int cur = 0; cur < 2 * size + 1; cur++) sum += triangle[row - size][col - cur];
+              sum += col - 2 * size - 1 == 0 ? pSum[row - size][col] : pSum[row - size][col] - pSum[row - size][col - 2 * size - 1];
               max = Math.max(sum, max);
             }
           }
         }
       }
-      bw.write(labeling++ + ". " + max + "\n");
+      bw.write((labeling++) + ". " + max + "\n");
     }
     bw.flush();
   }
