74e0b16d3a22b770b602a1dcdb89c0e9192966de
jhKim988 <fccva3910@gmail.com>
2021-12-15 15:17:33 +0900

    Algorithm Practice

--- Baekjoon/BOJ10531.java
+++ Baekjoon/BOJ10531.java
@@ -7,6 +7,7 @@ public class BOJ10531 {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    
     int N = Integer.parseInt(br.readLine());
     int[] shoot = new int[N];
     int max = 0;
@@ -15,13 +16,14 @@ public class BOJ10531 {
       shoot[i] = input;
       if (max < input) max = input;
     }
+
     int M = Integer.parseInt(br.readLine());
     int[] dist = new int[M];
     for (int i = 0; i < M; i++) dist[i] = Integer.parseInt(br.readLine());
 
     int[] poly = new int[max + 1];
     for (int i = 0; i < N; i++) poly[shoot[i]] = 1;
-
+    
     int[] product = polynomialProduct(poly, poly);
     int sum = 0;
     for (int i = 0; i < M; i++) {
@@ -29,22 +31,6 @@ public class BOJ10531 {
     }
     bw.write(sum + "\n");
     bw.flush();
-  }
-  static String printPoly(double[][] poly) {
-    StringBuilder sb = new StringBuilder();
-    sb.append('(').append(poly[0][0]).append(" + ").append(poly[0][1]).append("i)");
-    for (int i = 1; i < poly.length; i++) {
-      sb.append(" +(").append(poly[i][0]).append(" + ").append(poly[i][1]).append("i)x^").append(i);
-    }
-    return sb.toString();
-  }
-  static String printPoly(int[] poly) {
-    StringBuilder sb = new StringBuilder();
-    sb.append(poly[0]);
-    for (int i = 1; i < poly.length; i++) {
-      sb.append(" + ").append(poly[i]).append("x^").append(i);
-    }
-    return sb.toString();
   }
   static int[] polynomialProduct(int[] a, int[] b) {
     int len = a.length + b.length - 1;
@@ -66,25 +52,14 @@ public class BOJ10531 {
     for (int i = 0; i < a.length; i++) la[i][0] = a[i];
     for (int i = 0; i < b.length; i++) lb[i][0] = b[i];
     
-    double[][] fa = fft(la, exp, idx);
-    double[][] fb = fft(lb, exp, idx);
+    double[][] fa = fft(la, exp, idx, false);
+    double[][] fb = fft(lb, exp, idx, false);
     double[][] result = new double[exp][2];
     for (int i = 0; i < exp; i++) {
       result[i][0] = fa[i][0] * fb[i][0] - fa[i][1] * fb[i][1];
       result[i][1] = fa[i][0] * fb[i][1] + fa[i][1] * fb[i][0];
     }
-    result = fft(result, exp, idx);
-    for (int i = 0; i < exp/2; i++) {
-      result[i][0] /= exp;
-      result[i][1] /= exp;
-      result[exp - i - 1][0] /= exp;
-      result[exp - i - 1][1] /= exp;
-    }
-    for (int i = 1; i < exp/2; i++) {
-      double[] tmp = result[i];
-      result[i] = result[exp - i];
-      result[exp - i] = tmp;
-    }
+    result = fft(result, exp, idx, true);
     int[] round = new int[exp];
     for (int i = 0; i < exp; i++) round[i] = (int) Math.round(result[i][0]);
     return round;
@@ -105,7 +80,6 @@ public class BOJ10531 {
     double[][] feven = fft_recursive(even, n/2);
     double[][] fodd = fft_recursive(odd, n/2);
     double[][] result = new double[n][2];
-
     for (int i = 0; i < n/2; i++) {
       double reUnit = Math.cos(-2 * Math.PI * i / n);
       double imUnit = Math.sin(-2 * Math.PI * i / n);
@@ -116,41 +90,32 @@ public class BOJ10531 {
     }
     return result;
   }
-  static double[][] fft(double[][] poly, int n, int idx) {
+  static double[][] fft(double[][] poly, int n, int idx, boolean isInverse) {
     // Bit-reverse
-    int j = 0;
-    int n2 = n/2;
-    for (int i = 1; i < n - 1; i++) {
-      int n1 = n2;
-      while (j >= n1) {
-        j -= n1;
-        n1 /= 2;
+    for (int i = 1, rev = 0; i < n; i++) {
+      int bit = n >> 1;
+      while (rev >= bit) {
+        rev -= bit;
+        bit >>= 1;
       }
-      j += n1;
-    
-      if (i < j) {
-        double t1 = poly[i][0];
-        poly[i][0] = poly[j][0];
-        poly[j][0] = t1;
-        t1 = poly[i][1];
-        poly[i][1] = poly[j][1];
-        poly[j][1] = t1; 
+      rev += bit;
+      if (i < rev) {
+        double[] tmp = poly[i];
+        poly[i] = poly[rev];
+        poly[rev] = tmp;
       }
     }
-
     // FFT
     int n1 = 0;
-    n2 = 1;
-    
+    int n2 = 1;    
     for (int i = 0; i < idx; i++) {
       n1 = n2;
       n2 += n2;
       int a = 0;
-      for (j = 0; j < n1; j++) {
+      for (int j = 0; j < n1; j++) {
         double c = cos[a];
-        double s = sin[a];
+        double s = isInverse ? -sin[a] : sin[a];
         a += 1 << (idx - i - 1);
-
         for (int k = j; k + n1 < n; k += n2) {
           double t1 = c * poly[k + n1][0] - s * poly[k + n1][1];
           double t2 = s * poly[k + n1][0] + c * poly[k + n1][1];
@@ -161,7 +126,12 @@ public class BOJ10531 {
         }
       }
     }
-
+    if (isInverse) {
+      for (int i = 0; i < n; i++) {
+        poly[i][0] /= n;
+        poly[i][1] /= n;
+      }
+    }
     return poly;
   }
   static double[][] dft(double[][] poly, int n) {
@@ -182,5 +152,21 @@ public class BOJ10531 {
       }
     }
     return result;
+  }
+  static String printPoly(double[][] poly) {
+    StringBuilder sb = new StringBuilder();
+    sb.append('(').append(poly[0][0]).append(" + ").append(poly[0][1]).append("i)");
+    for (int i = 1; i < poly.length; i++) {
+      sb.append(" +(").append(poly[i][0]).append(" + ").append(poly[i][1]).append("i)x^").append(i);
+    }
+    return sb.toString();
+  }
+  static String printPoly(int[] poly) {
+    StringBuilder sb = new StringBuilder();
+    sb.append(poly[0]);
+    for (int i = 1; i < poly.length; i++) {
+      sb.append(" + ").append(poly[i]).append("x^").append(i);
+    }
+    return sb.toString();
   }
 }
