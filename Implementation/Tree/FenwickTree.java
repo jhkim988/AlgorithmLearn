20cfed0d7f9de3a6844eb46c2d7f6cffcd1d8978
jhkim988 <fccva3910@gmail.com>
2022-01-23 18:37:43 +0900

    Algorithm Implementation: Fenwick Tree

---
+++ Implementation/Tree/FenwickTree.java
@@ -0,0 +1,26 @@
+public class FenwickTree {
+  public static void main(String[] args) {
+    int len = 10;
+    int[] arr = new int[len];
+
+    // arr value allocation...
+
+    int[] tree = new int[len + 1];
+    // tree[i]: A[i] + ... (the number of sum: i & -i)
+
+  }
+  static int sum(int[] tree, int i) {
+    int value = 0;
+    while (i > 0) {
+      value += tree[i];
+      i -= i & -i;
+    }
+    return value;
+  }
+  static void update(int[] tree, int i, int diff) {
+    while (i < tree.length) {
+      tree[i] += diff;
+      i += i & -i;
+    }
+  }
+}
