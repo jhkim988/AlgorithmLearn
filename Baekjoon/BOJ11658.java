e3f984faded6ca1bc47ff7d9b814f6b15fe2e5f4
jhkim988 <fccva3910@gmail.com>
2022-01-23 22:50:01 +0900

    Algorithm Practice: 2D Fenwick Tree

--- Baekjoon/BOJ11658.java
+++ Baekjoon/BOJ11658.java
@@ -16,69 +16,54 @@ public class BOJ11658 {
     StringTokenizer st = new StringTokenizer(br.readLine());
     int size = Integer.parseInt(st.nextToken());
     int numQuery = Integer.parseInt(st.nextToken());
-    int[][] data = new int[size][size];
-    for (int i = 0; i < size; i++) {
+    int[][] data = new int[size + 1][size + 1];
+    int[][] tree = new int[size + 1][size + 1];
+
+    for (int i = 1; i <= size; i++) {
       st = new StringTokenizer(br.readLine());
-      for (int j = 0; j < size; j++) {
+      for (int j = 1; j <= size; j++) {
         data[i][j] = Integer.parseInt(st.nextToken());
+        update(tree, i, j, data[i][j]);
       }
     }
-
-    int height = 0;
-    while ((1 << height) < size) height++;
-    int treeSize = 1 << (height + 1);
-    long[][] forest = new long[size][treeSize];
-    for (int i = 0; i < size; i++) {
-      init(data[i], forest[i], 1, 0, size - 1);
-    }
-
     while (numQuery-- > 0) {
       st = new StringTokenizer(br.readLine());
       int type = Integer.parseInt(st.nextToken());
       if (type == 0) { // change
-        int row = Integer.parseInt(st.nextToken()) - 1;
-        int col = Integer.parseInt(st.nextToken()) - 1;
+        int row = Integer.parseInt(st.nextToken());
+        int col = Integer.parseInt(st.nextToken());
         int value = Integer.parseInt(st.nextToken());
         int diff = value - data[row][col];
         data[row][col] = value;
-        update(forest[row], 1, 0, size - 1, col, diff);
+        update(tree, row, col, diff);
       } else { // get Partial Sum
-        int firstRow = Integer.parseInt(st.nextToken()) - 1;
-        int firstCol = Integer.parseInt(st.nextToken()) - 1;
-        int secondRow = Integer.parseInt(st.nextToken()) - 1;
-        int secondCol = Integer.parseInt(st.nextToken()) - 1;
-        long value = 0;
-        for (int rowIdx = firstRow; rowIdx <= secondRow; rowIdx++) {
-          value += sum(forest[rowIdx], 1, 0, size - 1, firstCol, secondCol);
-        }
-        bw.write(Long.toString(value));
+        int firstRow = Integer.parseInt(st.nextToken());
+        int firstCol = Integer.parseInt(st.nextToken());
+        int secondRow = Integer.parseInt(st.nextToken());
+        int secondCol = Integer.parseInt(st.nextToken());        
+        bw.write(Integer.toString(pSum(tree, firstRow - 1, firstCol - 1, secondRow, secondCol)));
         bw.newLine();
       }
     }
     bw.flush();
   }
-  static long init(int[] arr, long[] tree, int node, int start, int end) {
-    if (start  == end) return tree[node] = arr[start];
-    int mid = (start + end) / 2;
-    long leftChild = init(arr, tree, node*2, start, mid);
-    long rightChild = init(arr, tree, node*2+1, mid+1, end);
-    return tree[node] = leftChild + rightChild;
-  }
-  static void update(long[] tree, int node, int start, int end, int index, int diff) {
-    if (start > index || end < index) return;
-    tree[node] += diff;
-    if (start != end) {
-      int mid = (start + end) / 2;
-      update(tree, node*2, start, mid, index, diff);
-      update(tree, node*2+1, mid+1, end, index, diff);
+  static int getSum(int[][] tree, int row, int col) {
+    int ret = 0;
+    for (int i = row; i > 0; i -= i & -i) {
+      for (int j = col; j > 0; j -= j & -j) {
+        ret += tree[i][j];
+      }
     }
+    return ret;
   }
-  static long sum(long[] tree, int node, int start, int end, int left, int right) {
-    if (start > right || end < left) return 0L;
-    if (left <= start && end <= right) return tree[node];
-    int mid = (start + end) / 2;
-    long leftChild = sum(tree, node*2, start, mid, left, right);
-    long rightChild = sum(tree, node*2+1, mid+1, end, left, right);
-    return leftChild + rightChild;
+  static int pSum(int[][] tree, int row1, int col1, int row2, int col2) {
+    return getSum(tree, row2, col2) - getSum(tree, row2, col1) - getSum(tree, row1, col2) + getSum(tree, row1, col1);
+  }
+  static void update(int[][] tree, int row, int col, int diff) {
+    for (int i = row; i < tree.length; i += i & -i) {
+      for (int j = col; j < tree[i].length; j += j & -j) {
+        tree[i][j] += diff;
+      }
+    }
   }
 }
