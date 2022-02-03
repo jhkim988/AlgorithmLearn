4172dc046db624f362016567bcc0b5c30601ba31
jhkim988 <fccva3910@gmail.com>
2022-02-03 23:32:14 +0900

    Algorithm

--- Implementation/Graph/GraphWeighted.java
+++ Implementation/Graph/GraphWeighted.java
@@ -8,10 +8,15 @@ public class GraphWeighted {
       graph.add(new LinkedList<>());
     }
   }
+  void addEdge(int a, int b, int w, boolean bidirectional) {
+    if (bidirectional) {
+      graph.get(b).add(new Edge(a, w));
+    }
+    graph.get(a).add(new Edge(b, w));
+  }
   void addEdge(int a, int b, int w) {
     graph.get(a).add(new Edge(b, w));
-    graph.get(b).add(new Edge(a, w));
-  }
+    graph.get(b).add(new Edge(a, w));}
   Queue<Edge> get(int i) {
     return graph.get(i);
   }
