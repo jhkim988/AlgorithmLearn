c5fb464f78be6803011c6b9f37d6a66cecd643f6
jhkim988 <fccva3910@gmail.com>
2022-02-07 02:07:34 +0900

    Algorithm Practice: FFT Application

--- Baekjoon/BOJ17104.java
+++ Baekjoon/BOJ17104.java
@@ -1,56 +1,75 @@
 import java.io.*;
+import java.util.*;
 
 public class BOJ17104 {
+  static double[] cos;
+  static double[] sin;
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    final int lim = 1_000_001;
-    final int fftLen = 1 << 21;
-    int[] notprime = new int[lim];
-    double[][] p = new double[fftLen][2];
-    notprime[0] = notprime[1] = 1;
-    for (int i = 2; i < lim; i++) {
-      if (notprime[i] == 1) continue;
-      p[i][0] = 1;
-      for (int ptr = i * 2; ptr < lim; ptr += i) {
-        notprime[ptr] = 1;
+    final int lim = 500_000;
+    int[] prime = new int[1_000_000];
+    int[] primeGenerator = new int[lim]; // p = 2a + 1
+    Arrays.fill(prime, 1);
+    prime[0] = prime[1] = 0;
+    for (int i = 2; i*i <= prime.length; i++) {
+      if (prime[i] == 0) continue;
+      for (int ptr = i+i; ptr < prime.length; ptr += i) {
+        prime[ptr] = 0;
       }
     }
-    double[][] result = square(p);
+
+    for (int i = 0; i < prime.length; i++) {
+      if (prime[i] == 0) continue;
+      primeGenerator[(i - 1) >> 1] = 1;
+    }
+    primeGenerator[0] = 0;
+
+    int[] sq = square(primeGenerator);
     int numTest = Integer.parseInt(br.readLine());
     while (numTest-- > 0) {
       int t = Integer.parseInt(br.readLine());
-      if (notprime[t >> 1] == 1) {
-        bw.write(Integer.toString(((int) Math.round(result[t][0])) / 2));
+      if (t == 4) {
+        bw.write('1');
+      } else if (prime[t>>1] == 0) {
+        bw.write(Integer.toString(((int) Math.round(sq[(t>>1) - 1])) / 2));
       } else {
-        bw.write(Integer.toString(((int) Math.round(result[t][0]) - 1) / 2 + 1));
+        bw.write(Integer.toString(((int) Math.round(sq[(t>>1) - 1]) - 1) / 2 + 1));
       }
       bw.newLine();
     }
     bw.flush();
   }
-  static double[][] square(double[][] p) {
-    int fftLen = 1 << 21;
-    int trigInitLen = fftLen >> 2;
-    double angle = Math.PI/(trigInitLen >> 1);
-    double[] cos = new double[trigInitLen];
-    double[] sin = new double[trigInitLen];
+  static int[] square(int[] _p) {
+    int fftLen = 1;
+    while (fftLen < _p.length + _p.length) fftLen <<= 1;
+    int trigInitLen = fftLen >> 1;
+    double angle = Math.PI/trigInitLen;
+    cos = new double[trigInitLen];
+    sin = new double[trigInitLen];
     for (int i = 0; i < trigInitLen; i++) {
       cos[i] = Math.cos(angle * i);
       sin[i] = Math.sin(angle * i);
     }
-    
-    fft(p, false, cos, sin);
+    double[][] p = new double[fftLen][2];
+    for (int i = 0; i < _p.length; i++) {
+      p[i][0] = _p[i];
+    }
+    fft(p, false);
     for (int i = 0; i < fftLen; i++) {
       double re = p[i][0]*p[i][0] - p[i][1]*p[i][1];
       double im = p[i][0]*p[i][1] + p[i][1]*p[i][0];
       p[i][0] = re;
       p[i][1] = im;
     }
-    fft(p, true, cos, sin);
-    return p;
+    fft(p, true);
+    int[] r = new int[_p.length + _p.length - 1];
+    for (int i = 0; i < r.length; i++) {
+      r[i] = (int) Math.round(p[i][0]);
+    }
+    return r;
   }
-  static void fft(double[][] p, boolean isInverse, double[] cos, double[] sin) {
+  static void fft(double[][] p, boolean isInverse) {
     int n = p.length;
     int v = n >> 1;
     for (int i = 1, rev = 0; i < n; i++) {
@@ -71,9 +90,9 @@ public class BOJ17104 {
       for (int i = 0; i < n; i += 2*k) {
         for (int j = 0; j < k; j++) {
           int trigIdx = v / k * j;
-          if (trigIdx >= (v >> 1)) trigIdx = v - trigIdx;
           double c = cos[trigIdx];
-          double s = sin[trigIdx] * (isInverse ? -1 : 1);
+          double s = sin[trigIdx];
+          if (isInverse) s = -s;
           double[] p1 = p[i + j];
           double[] p2 = p[i + j + k];
           double rex = p1[0];
