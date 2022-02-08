07d82aa46269788b98c8debe97dedf1b52c8b286
jhkim988 <fccva3910@gmail.com>
2022-02-09 03:37:44 +0900

    Algorithm: SegmentTree

--- Baekjoon/BOJ12016.java
+++ Baekjoon/BOJ12016.java
@@ -2,15 +2,14 @@ import java.io.*;
 import java.util.*;
 
 public class BOJ12016 {
-  private static class SegmentTree {
+  private static class SegmentTreeMin {
     // node: index of minimum value on [i, j]
     int n, treeSize;
     int[] arr, tree;
-    SegmentTree(int[] arr) {
-      this.n = arr.length+1;
+    SegmentTreeMin(int[] arr) {
+      this.n = arr.length;
       this.arr = new int[this.n];
-      System.arraycopy(arr, 0, arr, 1, arr.length);
-      this.arr[0] = Integer.MAX_VALUE;
+      System.arraycopy(arr, 0, this.arr, 0, arr.length);
       treeSize = 1;
       while (treeSize < n) treeSize <<= 1;
       treeSize <<= 1;
@@ -22,10 +21,10 @@ public class BOJ12016 {
       int mid = (start + end) >> 1;
       int leftChild = init(node*2, start, mid);
       int rightChild = init(node*2+1, mid+1, end);
-      return tree[node] = arr[leftChild] < arr[rightChild] ? rightChild : leftChild; 
+      return tree[node] = arr[leftChild] > arr[rightChild] ? rightChild : leftChild; 
     }
     int update(int node, int start, int end, int idx, int val) {
-      if (start > idx || end < idx) return 0;
+      if (start > idx || end < idx) return tree[node];
       if (start == end) {
         arr[idx] = val;
         return start;
@@ -33,7 +32,7 @@ public class BOJ12016 {
       int mid = (start + end) >> 1;
       int leftChild = update(node*2, start, mid, idx, val);
       int rightChild = update(node*2+1, mid+1, end, idx, val);
-      return tree[node] = arr[leftChild] < arr[rightChild] ? rightChild : leftChild; 
+      return tree[node] = arr[leftChild] > arr[rightChild] ? rightChild : leftChild; 
     }
     int get(int node, int start, int end, int left, int right) {
       if (start > right || end < left) return 0;
@@ -41,12 +40,49 @@ public class BOJ12016 {
       int mid = (start + end) >> 1;
       int leftChild = get(node*2, start, mid, left, right);
       int rightChild = get(node*2+1, mid+1, end, left, right);
-      return arr[leftChild] < arr[rightChild] ? rightChild : leftChild;
+      return arr[leftChild] > arr[rightChild] ? rightChild : leftChild;
     }
     int removeMin() {
       int idx = tree[1];
       update(1, 0, n-1, idx, Integer.MAX_VALUE);
       return idx;
+    }
+  }
+  private static class SegmentTreeCount {
+    int n, treeSize;
+    int[] arr, tree;
+    SegmentTreeCount(int n) {
+      this.n = n;
+      treeSize = 1;
+      while (treeSize < this.n) treeSize <<= 1;
+      treeSize <<= 1;
+      arr = new int[this.n];
+      Arrays.fill(arr, 1);
+      tree = new int[treeSize];
+      init(1, 0, this.n-1);
+    }
+    int init(int node, int start, int end) {
+      if (start == end) return tree[node] = arr[start];
+      int mid = (start + end) >> 1;
+      int leftChild = init(node*2, start, mid);
+      int rightChild = init(node*2+1, mid+1, end);
+      return tree[node] = leftChild + rightChild;
+    }
+    int update(int node, int start, int end, int idx, int val) {
+      if (start > idx || end < idx) return tree[node];
+      if (start == end) return tree[node] = arr[start] = val;
+      int mid = (start + end) >> 1;
+      int leftChild = update(node*2, start, mid, idx, val);
+      int rightChild = update(node*2+1, mid+1, end, idx, val);
+      return tree[node] = leftChild + rightChild;
+    }
+    int get(int node, int start, int end, int left, int right) {
+      if (start > right || end < left) return 0;
+      if (left <= start && end <= right) return tree[node];
+      int mid = (start + end) >> 1;
+      int leftChild = get(node*2, start, mid, left, right);
+      int rightChild = get(node*2+1, mid+1, end, left, right);
+      return leftChild + rightChild;
     }
   }
   public static void main(String[] args) throws IOException {
@@ -59,6 +95,33 @@ public class BOJ12016 {
       arr[i] = Integer.parseInt(st.nextToken());
     }
 
-    SegmentTree sg = new SegmentTree(arr);
+    SegmentTreeMin sgm = new SegmentTreeMin(arr);
+    SegmentTreeCount sgc = new SegmentTreeCount(n);
+    int[] time = new int[n];
+    int prevIdx = 0;
+    int remainWork = n;
+    while (remainWork-- > 0) {
+      int idx = sgm.removeMin();
+      int count = 0;
+      if (arr[idx] == arr[prevIdx]) {
+        count = time[prevIdx] + sgc.get(1, 0, n-1, prevIdx, idx);
+      } else {
+        if (remainWork == 0) {
+          count = time[prevIdx] + arr[idx] - arr[prevIdx] + (prevIdx < idx ? 1 : 0);
+        } else {
+          count = time[prevIdx] + sgc.get(1, 0, n-1, prevIdx, n-1) + sgc.get(1, 0, n-1, 0, idx);
+        }
+        count += arr[idx] - arr[prevIdx];
+      }
+      sgc.update(1, 0, n-1, idx, 0);
+      System.out.println("idx: " + idx + ", count: " + count);
+      time[idx] = count;
+      prevIdx = idx;
+    }
+    for (int t : time) {
+      bw.write(Integer.toString(t));
+      bw.newLine();
+    }
+    bw.flush();
   }
 }
