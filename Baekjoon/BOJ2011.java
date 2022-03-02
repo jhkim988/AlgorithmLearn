c9aad446b82d732bf18d9a34ae6b43a6b7e46c1f
jhkim988 <fccva3910@gmail.com>
2022-03-02 21:17:53 +0900

    Algorithm Practice

--- Baekjoon/BOJ2011.java
+++ Baekjoon/BOJ2011.java
@@ -7,33 +7,48 @@ public class BOJ2011 {
     char[] input = br.readLine().toCharArray();
     long[] dp = new long[input.length];
     if (input[0] == '0') {
-      bw.write("0\n");
-      bw.flush();
-      return;
-    } 
-
-    dp[0] = 1;
-    if ((input[0] >= '3' || input[0] == '0') && input[1] == '0') {
       fail();
       return;
     }
-    if (input[0] == '1' || (input[0] == '2' && input[1] <= '6')) {
-      dp[1] = 2;
-    } else {
-      dp[1] = 1;
+    if (input.length == 1) {
+      bw.write("1\n");
+      bw.flush();
+      return;
     }
-    for (int i = 2; i < input.length; i++) {
-      if ((input[i-1] >= '3' || input[i-1] == '0') && input[i] == '0') {
+    dp[0] = 1;
+    if (input[1] == '0') {
+      if (input[0] == '1' || input[0] == '2') {
+        dp[1] = dp[0];
+      } else {
         fail();
         return;
       }
-      if (input[i-1] == '1' || (input[i-1] == '2' && input[i] <= '6')) {
-        dp[i] = (dp[i-1] + dp[i-2]) % 1_000_000;
+    }  else {
+      if (input[0] >= '3' || (input[0] == '2' && input[1] >= '7')) {
+        dp[1] = 1;
       } else {
-        dp[i] = dp[i-1];
+        dp[1] = 2;
       }
     }
 
+
+    for (int i = 2; i < input.length; i++) {
+      if (input[i] == '0') {
+        if (input[i-1] == '1' || input[i-1] == '2') {
+          dp[i] = dp[i-2];
+        } else {
+          fail();
+          return;
+        }
+      } else {
+        if (input[i-1] == '0' || input[i] >= '7' && input[i-1] == '2' || input[i-1] >= '3') {
+          dp[i] = dp[i-1];
+        } else {
+          dp[i] = (dp[i-1] + dp[i-2]) % 1_000_000;
+        }
+      }
+    }
+    
     bw.write(Long.toString(dp[input.length-1]));
     bw.newLine();
     bw.flush();
