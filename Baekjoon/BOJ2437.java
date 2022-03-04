c2c15612a07ba3a7c54f399bf8dc86da3315f95f
jhKim988 <fccva3910@gmail.com>
2022-03-04 13:58:55 +0900

    Algorithm Practice

---
+++ Baekjoon/BOJ2437.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ2437 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    int n = Integer.parseInt(br.readLine());
+    int[] arr = new int[n];
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
+    }
+    
+  }
+}
