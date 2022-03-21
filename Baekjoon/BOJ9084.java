7d5dfd3083b72ce32458621b4fc70bcad86248fc
jhKim988 <fccva3910@gmail.com>
2022-03-21 15:35:42 +0900

    Algorithm Practice: Dynamic Programming

--- Baekjoon/BOJ9084.java
+++ Baekjoon/BOJ9084.java
@@ -14,19 +14,26 @@ public class BOJ9084 {
         types[i] = Integer.parseInt(st.nextToken());
       }
       int target = Integer.parseInt(br.readLine());
-      int[] dp = new int[target+1];
+      int[][] dp = new int[target + 1][numType];
+      for (int i = 0; i <= target; i++) Arrays.fill(dp[i], -1);
+      int answer = 0;
       for (int i = 0; i < numType; i++) {
-        if (types[i] <= target) dp[types[i]] = 1; 
+        answer += recur(target - types[i], i, types, dp);
       }
-      for (int i = 1; i <= target; i++) {
-        for (int j = 0; j < numType; j++) {
-          if (i >= types[j]) dp[i] += dp[i - types[j]];
-        }
-      }
-      bw.write(Integer.toString(dp[target]));
+      bw.write(Integer.toString(answer));
       bw.newLine();
       bw.flush();
-      System.out.println(Arrays.toString(dp));
     }
+  }
+  static int recur(int money, int use, int[] types, int[][] dp) {
+    if (money == 0) return 1;
+    if (money < 0) return 0;
+    if (dp[money][use] != -1) return dp[money][use];
+    dp[money][use] = 0;
+    for (int i = 0; i < types.length; i++) {
+      if (use <= i)
+        dp[money][use] += recur(money - types[i], i, types, dp);
+    }
+    return dp[money][use];
   }
 }
