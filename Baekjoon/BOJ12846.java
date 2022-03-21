5b6fe464c30701152d42deab9f5bd57b953c2062
jhKim988 <fccva3910@gmail.com>
2022-03-21 10:02:28 +0900

    Algorithm Practice

--- Baekjoon/BOJ12846.java
+++ Baekjoon/BOJ12846.java
@@ -16,25 +16,49 @@ public class BOJ12846 {
     }
     void init(int node, int start, int end) {
       if (start == end) {
-        min[node] = arr[start];
+        min[node] = start;
         return;
       }
       int mid = (start + end)>>1;
       init(node<<1, start, mid);
       init(node<<1|1, mid+1, end);
       min[node] = Integer.min(min[node<<1], min[node<<1|1]);
-    }
-    int findMax(int node, int start, int end) {
-      if (start == end) {
-        return arr[start];
+      if (arr[min[node<<1]] < arr[min[node<<1|1]]) {
+        min[node] = min[node<<1];
+      } else {
+        min[node] = min[node<<1|1];
       }
+    }
+    int min(int node, int start, int end, int left, int right) {
+      if (start > right || end < left) return -1;
+      if (left <= start && end <= right) return min[node];
       int mid = (start + end)>>1;
-      int leftVal = findMax(node<<1, start, mid);
-      int rightVal = findMax(node<<1|1, mid+1, end);
-      return Integer.max(min[node]*(end-start+1), Integer.max(leftVal, rightVal));
+      int idx1 = min(node<<1, start, mid, left, right);
+      int idx2 = min(node<<1|1, mid+1, end, left, right);
+      if (idx1 == -1) {
+        return idx2;
+      } else if (idx2 == -1) {
+        return idx1;
+      } else {
+        if (arr[idx1] < arr[idx2]) return idx1;
+        else return idx2;
+      }
+    }
+    int findMax(int left, int right) {
+      int idx = min(1, 0, n-1, left, right);
+      int area = arr[idx] * (right - left + 1);
+      if (left <= idx - 1) {
+        int leftMin = findMax(left, idx-1);
+        if (area < leftMin) area = leftMin;
+      }
+      if (right >= idx + 1) {
+        int rightMin = findMax(idx+1, right);
+        if (area < rightMin) area = rightMin;
+      }
+      return area;
     }
     int findMax() {
-      return findMax(1, 0, n-1);
+      return findMax(0, n-1);
     }
   }
   public static void main(String[] args) throws IOException {
@@ -47,7 +71,8 @@ public class BOJ12846 {
       arr[i] = Integer.parseInt(st.nextToken());
     }
     SegTree sg = new SegTree(arr);
-    bw.write(Integer.toString(sg.findMax()));
+    int answer = sg.findMax();
+    bw.write(Integer.toString(answer));
     bw.newLine();
     bw.flush();
   }  
