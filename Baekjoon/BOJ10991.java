6994da2022c66d828d5e30658a39f98f4372bdd3
jhkim988 <fccva3910@gmail.com>
2022-04-10 23:27:12 +0900

    Algorithm

---
+++ Baekjoon/BOJ10991.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class BOJ10991 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    int n = Integer.parseInt(br.readLine());
+    for (int i = 1; i <= n; i++) {
+      for (int j = 0; j < n-i; j++) {
+        bw.write(' ');
+      }
+      for (int j = 0; j < i; j++) {
+        bw.write('*');
+        bw.write(' ');
+      }
+      bw.newLine();
+    }
+    bw.flush();
+  }  
+}
