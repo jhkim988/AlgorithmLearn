b53b5954a0484d01dca2634869a34b09017a9757
jhKim988 <fccva3910@gmail.com>
2022-04-11 14:37:44 +0900

    Algorithm

--- Baekjoon/BOJ2212.java
+++ Baekjoon/BOJ2212.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.util.*;
-import java.util.stream.*;
 
 public class BOJ2212 {
   public static void main(String[] args) throws IOException {
@@ -13,40 +12,22 @@ public class BOJ2212 {
     for (int i = 0; i < n; i++) {
       censor[i] = Integer.parseInt(st.nextToken());
     }
-    censor = IntStream.of(censor).sorted().distinct().toArray();
-    System.out.println(Arrays.toString(censor));
-    int answer = greedy(censor, 0, k);
+    Arrays.sort(censor);
+    ArrayList<Integer> tmp = new ArrayList<>();
+    int answer = 0;
+    for (int i = 1; i < n; i++) {
+      answer += censor[i] - censor[i-1];
+      tmp.add(censor[i]- censor[i-1]);
+    }
+    Collections.sort(tmp, Collections.reverseOrder());
+    int ptr = 0;
+    int remainder = 1;
+    while (ptr < n-1 && remainder < k) {
+      answer -= tmp.get(ptr++);
+      remainder++;
+    }
     bw.write(Integer.toString(answer));
     bw.newLine();
     bw.flush();
-  }
-  static int greedy(int[] arr, int start, int remainder) {
-    if (start == arr.length) {
-      return 3_000_000;
-    }
-    if (remainder == 1) {
-      System.out.println("[" + start + ", " + (arr.length-1) + "]");
-      return arr[arr.length-1] - arr[start];
-    }
-    double num = 1;
-    double density = 0;
-    int idx = start;
-    for (int i = start+1; i < arr.length; i++) {
-      num++;
-      if (density <= num/(arr[i]-arr[start])) {
-        idx = i;
-        density = num/(arr[i]-arr[start]); 
-      } 
-    }
-    System.out.println("start: " + start + ", density: " + density + ", idx: " + idx);
-    int zero = greedy(arr, start+1, remainder-1);
-    int nonzero = arr[idx]-arr[start] + greedy(arr, idx+1, remainder-1);
-    if (nonzero < zero) {
-      System.out.println("[" + start + ", " + idx + "]");
-      return nonzero;
-    } else {
-      System.out.println("[" + start + ", " + start + "]");
-      return zero;
-    }
   }
 }
