ba2ff50d47356440ecf9cd033423dcd83475ccf1
jhKim988 <fccva3910@gmail.com>
2022-04-14 11:02:46 +0900

    Greedy is TOO HARD

--- Baekjoon/BOJ1700.java
+++ Baekjoon/BOJ1700.java
@@ -9,39 +9,51 @@ public class BOJ1700 {
     int capacity = Integer.parseInt(st.nextToken());
     int numTask = Integer.parseInt(st.nextToken());
     st = new StringTokenizer(br.readLine());
-    int[] task = new int[numTask];
-    int[] numAppear = new int[numTask+1];
+    int[] taskque = new int[numTask];
+    int[] numPlug = new int[numTask+1];
     for (int i = 0; i < numTask; i++) {
-      task[i] = Integer.parseInt(st.nextToken());
+      taskque[i] = Integer.parseInt(st.nextToken());
+      numPlug[taskque[i]]++;
     }
-    for (int i = 0; i < capacity; i++) {
-      numAppear[task[i]]++;
-    }
-    int current = 0;
     int numUnplug = 0;
-    boolean[] plug = new boolean[numTask+1];
+    int totPlug = 0;
+    boolean[] onPlug = new boolean[numTask+1];
+    int[] rank = new int[numTask+1];
     for (int i = 0; i < numTask; i++) {
-      if (plug[task[i]]) {
-        // System.out.println("already plug: " + task[i]);
-        continue;
-      } else if (current < capacity) {
-        plug[task[i]] = true;
-        current++;
-      } else {
-        int minIdx = numTask+1;
-        for (int j = 1; j <= numTask; j++) {
-          if (!plug[j]) continue;
-          if (numAppear[j] < minIdx) minIdx = j;
+      if (!onPlug[taskque[i]]) {
+        if (totPlug < capacity) { // there is free hole
+          totPlug++;
+          // System.out.println("plug in free hole: " + taskque[i]);
+        } else { // no free hole(must unplug)
+          int zero = -1;
+          int last = i;
+          Arrays.fill(rank, numTask+2);
+          for (int j = i; j < numTask; j++) {
+            if (!onPlug[taskque[j]]) continue;
+            if (j < rank[taskque[j]]) {
+              rank[taskque[j]] = j;
+              last = taskque[j];
+            }            
+          }
+          for (int j = 1; j <= numTask; j++) {
+            if (!onPlug[j]) continue;
+            if (numPlug[j] == 0) zero = j;
+          }
+          if (zero != -1) {
+            // System.out.println("unplug(no use): " + zero);
+            onPlug[zero] = false;
+          } else {
+            // System.out.println("unplug(last use): " + last);
+            onPlug[last] = false;
+          }
+          numUnplug++;
+          // System.out.println("plug after unplug: " + taskque[i]);
         }
-        numUnplug++;
-        plug[minIdx] = false;
-        plug[task[i]] = true;
-        // System.out.println("unplug: " + minIdx);
+        onPlug[taskque[i]] = true;
+      } else {
+        // System.out.println("already plug: " + taskque[i]);
       }
-      // System.out.println("plug: " + task[i]);
-      // System.out.println(Arrays.toString(numAppear));
-      numAppear[task[i]]--;
-      if (i + capacity < numTask) numAppear[task[i + capacity]]++;
+      numPlug[taskque[i]]--;
     }
     bw.write(Integer.toString(numUnplug));
     bw.newLine();
