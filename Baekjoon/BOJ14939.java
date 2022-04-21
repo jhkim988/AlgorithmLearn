a6a94ed9ecb107549b724538b80430c6c490b882
jhKim988 <fccva3910@gmail.com>
2022-04-21 16:19:52 +0900

    Algorithm: BruteForce + Greedy

--- Baekjoon/BOJ14939.java
+++ Baekjoon/BOJ14939.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.*;
 
 public class BOJ14939 {
   static int[] rowDi = {0, -1, 0, 1, 0};
@@ -10,7 +11,9 @@ public class BOJ14939 {
     for (int i = 0; i < 10; i++) {
       input[i] = br.readLine().toCharArray();
     }
-    recur(0, 0, input, 0);
+    bw.write(Integer.toString(recur(0, 0, input, 0)));
+    bw.newLine();
+    bw.flush();
   }
   static void inversion(int row, int col, char[][] input) {
     for (int i = 0; i < 5; i++) {
@@ -22,10 +25,11 @@ public class BOJ14939 {
     }
   }
   static int recur(int row, int col, char[][] input, int numInversion) {
-    int min = 101;
     if (col == 10) {
       row++; col = 0;
     }
+    if (row == 10) return numInversion;
+    int min = 101;
     if (row == 0) {
       min = Integer.min(min, recur(row, col+1, input, numInversion));
       inversion(row, col, input);
@@ -33,16 +37,31 @@ public class BOJ14939 {
       inversion(row, col, input);
       return min;
     }
+    Queue<Integer> que = new LinkedList<>();
     int addInversion = 0;
     for (int r = 1; r < 10; r++) {
       for (int c = 0; c < 10; c++) {
         if (input[r-1][c] == 'O') {
           addInversion++;
           inversion(r, c, input);
-          
+          que.add(r*10+c);
         }
       }
     }
-    return min;
+    
+    for (int c = 0; c < 10; c++) {
+      if (input[9][c] == 'O') {
+        while (!que.isEmpty()) {
+          int ptr = que.poll();
+          inversion(ptr/10, ptr%10, input);
+        }
+        return 101;
+      }
+    }
+    while (!que.isEmpty()) {
+      int ptr = que.poll();
+      inversion(ptr/10, ptr%10, input);
+    }
+    return numInversion + addInversion;
   }
 }
