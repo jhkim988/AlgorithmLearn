c0d7805cf5fc0c08aa6b5fdc7c8811621dfc3f0c
jhKim988 <fccva3910@gmail.com>
2022-05-13 09:52:53 +0900

    Algorithm

--- Baekjoon/BOJ1405.java
+++ Baekjoon/BOJ1405.java
@@ -2,43 +2,35 @@ import java.io.*;
 import java.util.*;
 
 public class BOJ1405 {
+  static int n;
   static double[] prob;
-  private static class Pair {
-    int x, y;
-    Pair(int x, int y) {
-      this.x = x;
-      this.y = y;
-    }
-    @Override
-    public boolean equals(Object other) {
-      if (this == other) return true;
-      if (other.getClass() == this.getClass()) return true;
-      Pair o = (Pair) other;
-      return this.x == o.x && this.y == o.y;
-    }
-    @Override
-    public int hashCode() {
-      int hash = 7;
-      hash = 31*hash + x;
-      hash = 31*hash + y;
-      return hash;
-    }
-  }
+  static boolean[][] visit = new boolean[29][29];
+  static int[] xDi = {0, 0, -1, 1};
+  static int[] yDi = {1, -1, 0, 0};
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     StringTokenizer st = new StringTokenizer(br.readLine());
-    int n = Integer.parseInt(st.nextToken());
+    n = Integer.parseInt(st.nextToken());
     prob = new double[4];
     for (int i = 0; i < 4; i++) {
       prob[i] = Double.parseDouble(st.nextToken()) * 0.01;
     }
-
+    visit[14][14] = true;
+    double p = recur(0, 14, 14, 1.0);
+    bw.write(Double.toString(p));
+    bw.flush();
   }
-  static double recur(int depth, Pair stat, double p) {
+  static double recur(int depth, int x, int y, double p) {
+    if (depth >= n) return 1.0;
+    double crntP = 0;
     for (int i = 0; i < 4; i++) {
-
+      int nextX = x + xDi[i], nextY = y + yDi[i];
+      if (visit[nextX][nextY]) continue;
+      visit[nextX][nextY] = true;
+      crntP += prob[i]*recur(depth+1, nextX, nextY, p);
+      visit[nextX][nextY] = false;
     }
-    return 0;
+    return crntP;
   }
 }
