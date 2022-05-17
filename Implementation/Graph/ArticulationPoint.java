66fb1845d1aa9c2be5485e3c718438bf89a9036b
jhKim988 <fccva3910@gmail.com>
2022-05-17 11:51:12 +0900

    Graph/ArticulationPoints

--- Implementation/Graph/ArticulationPoint.java
+++ Implementation/Graph/ArticulationPoint.java
@@ -20,14 +20,16 @@ public class ArticulationPoint {
     int min = id, numChild = 0;
     dfsId[node] = id++;
     for (int adj : graph.get(node)) {
-      if (dfsId[adj] == 0) {
+      if (dfsId[adj] == 0) { // 자손이라면
         numChild++;
-        int low = dfs(adj, false);
-        if (!isRoot && low >= dfsId[node]) isAP[node] = true;
-      } else {
+        int low = dfs(adj, false); // adj와 연결된 가장 작은 dfsId
+        if (!isRoot && low >= dfsId[node]) isAP[node] = true; // node의 선조와 adj가 연결돼 있지 않는다면
+        min = Integer.min(min, low);
+      } else { 
         min = Integer.min(min, dfsId[adj]);
       }
     }
+    // Root에 자손이 2개 이상 있으면 단절점이다.
     if (isRoot) isAP[node] = numChild >= 2;
     return min;
   }
