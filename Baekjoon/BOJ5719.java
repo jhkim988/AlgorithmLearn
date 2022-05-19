c4e3fcec7386d3fbca0842a45b9b2639176bbb78
jhKim988 <fccva3910@gmail.com>
2022-05-19 16:23:06 +0900

    Algorithm

---
+++ Baekjoon/BOJ5719.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ5719 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int v = Integer.parseInt(st.nextToken());
+    int e = Integer.parseInt(st.nextToken());
+    while (v != 0 && e != 0) {
+      ArrayList<HashSet<Integer>> graph = new ArrayList<>();
+    }
+  }
+}
