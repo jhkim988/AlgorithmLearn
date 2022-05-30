482956732f8b19ef45b1911aefb4ca6c6fe4238a
jhKim988 <fccva3910@gmail.com>
2022-05-30 16:03:40 +0900

    Algorithm

--- Baekjoon/BOJ1006.java
+++ Baekjoon/BOJ1006.java
@@ -18,47 +18,60 @@ public class BOJ1006 {
         }
       }
 
-      // the number of 2*n tile.. (maximize)
-      int[][][] dp = new int[4][3][n]; // [init stat][i'th stat][i]
-      dp[0][0][0] = input[0][0]+input[1][0] <= w ? 1 : 0;
-      dp[1][1][0] = input[0][n-1]+input[0][0] <= w ? 1 : 0;
-      dp[2][2][0] = input[1][n-1]+input[1][0] <= w ? 1 : 0;
-      dp[3][0][0] = (input[0][n-1]+input[0][0] <= w && input[1][n-1]+input[1][0] <= w) ? 2 : 0;
-
-      dp[0][1][1] = input[0][0]+input[0][1] <= w ? 1 : 0;
-      dp[0][2][1] = input[1][0]+input[1][1] <= w ? 1 : 0;
-      dp[0][0][1] = Integer.max(dp[0][0][1], dp[0][0][0]+(input[0][1]+input[1][1] <= w ? 1 : 0));
-      dp[0][0][1] = Integer.max(dp[0][0][1], (input[0][0]+input[0][1] <= w && input[1][0]+input[1][1] <= w) ? 2 : 0);
-
-      for (int j = 0; j < 3; j++) {
-        dp[1][0][1] = Integer.max(dp[1][0][1], dp[1][j][0] + (input[0][1]+input[1][1] <= w ? 1 : 0));
-        dp[2][0][1] = Integer.max(dp[2][0][1], dp[2][j][0] + (input[0][1]+input[1][1] <= w ? 1 : 0));
+      if (n == 1) {
+        bw.write(input[0][0]+input[1][0] <= w ? "1\n" : "2\n");
+        continue;
       }
 
-      dp[1][2][1] = dp[1][1][0] + (input[1][0]+input[1][1] <= w ? 1 : 0);
-      dp[2][1][1] = dp[2][2][0] + (input[0][0]+input[0][1] <= w ? 1 : 0);
-      
+      // the number of 2*n tile.. (maximize)
+      int[][][] dp = new int[4][3][n]; // [init stat][i'th stat][i]
+      dp[0][0][0] = (input[0][0]+input[1][0] <= w) ? 1 : 0;
+      dp[1][1][0] = (input[0][n-1]+input[0][0] <= w) ? 1 : 0;
+      dp[2][2][0] = (input[1][n-1]+input[1][0] <= w) ? 1 : 0;
+      dp[3][0][0] = dp[1][1][0]+dp[2][2][0];
+
       for (int k = 0; k < 4; k++) {
-        for (int i = 2; i < n; i++) {
+        for (int i = 1; i < n; i++) {
+          for (int j = 0; j < 3; j++) {
+            dp[k][j][i] = dp[k][j][i-1];
+          }
           if (input[0][i]+input[1][i] <= w) {
             for (int j = 0; j < 3; j++) {
-              dp[k][0][i] = Integer.max(dp[k][j][i], dp[k][j][i-1]+1);
+              dp[k][0][i] = Integer.max(dp[k][0][i], dp[k][j][i-1]+1);
             }
           }
           if (input[0][i-1]+input[0][i] <= w && input[1][i-1]+input[1][i] <= w) {
-            for (int j = 0; j < 3; j++) {
-              dp[k][0][i] = Integer.max(dp[k][0][i], dp[k][j][i-2]+2);
+            if (i >= 2) {
+              for (int j = 0; j < 3; j++) {
+                dp[k][0][i] = Integer.max(dp[k][0][i], dp[k][j][i-2]+2);
+              }
+            } else {
+              for (int j = 0; j < 3; j++) {
+                dp[k][0][i] = Integer.max(dp[k][0][i], 2);
+              }
             }
           }
           if (input[0][i-1]+input[0][i] <= w) {
-            for (int j = 0; j < 3; j++) {
-              dp[k][1][i] = Integer.max(dp[k][1][i], dp[k][j][i-2]+1);
+            if (i >= 2) {
+              for (int j = 0; j < 3; j++) {
+                dp[k][1][i] = Integer.max(dp[k][1][i], dp[k][j][i-2]+1);
+              }
+            } else {
+              for (int j = 0; j < 3; j++) {
+                dp[k][1][i] = Integer.max(dp[k][1][i], 1);
+              }
             }
             dp[k][1][i] = Integer.max(dp[k][1][i], dp[k][2][i-1]+1);
           }
           if (input[1][i-1]+input[1][i] <= w) {
-            for (int j = 0; j < 3; j++) {
-              dp[k][2][i] = Integer.max(dp[k][2][i], dp[k][j][i-2]+1);
+            if (i >= 2) {
+              for (int j = 0; j < 3; j++) {
+                dp[k][2][i] = Integer.max(dp[k][2][i], dp[k][j][i-2]+1);
+              }
+            } else {
+              for (int j = 0; j < 3; j++) {
+                dp[k][2][i] = Integer.max(dp[k][2][i], 1);
+              }
             }
             dp[k][2][i] = Integer.max(dp[k][2][i], dp[k][1][i-1]+1);
           }
@@ -83,13 +96,16 @@ public class BOJ1006 {
       bw.write(Integer.toString(2*n-max));
       bw.newLine();
 
-      for (int j = 0; j < 2; j++) {
-        for (int i = 0; i < n; i++) {
-          System.out.print(dp[2][j][i] + " ");
-        }
-        System.out.println();
-      }
-
+      // for (int k = 0; k < 4; k++) {
+      //   System.out.println("init: " + k);
+      //   for (int j = 0; j < 3; j++) {
+      //     System.out.print("[");
+      //     for (int i = 0; i < n; i++) {
+      //       System.out.print(dp[k][j][i] + " ");
+      //     }
+      //     System.out.println("]");
+      //   }
+      // }
     }
     bw.flush();
   }
