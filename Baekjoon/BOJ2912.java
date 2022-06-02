51c6c297fb2413211650ef8fdd68c589528eda85
jhKim988 <fccva3910@gmail.com>
2022-06-02 11:37:05 +0900

    Algorithm: OfflineQuery(Mo's Algorithm), Randomize

--- Baekjoon/BOJ2912.java
+++ Baekjoon/BOJ2912.java
@@ -23,7 +23,8 @@ public class BOJ2912 {
       arr[i] = Integer.parseInt(st.nextToken());
     }
 
-    useMosAlgorithm(n, c, arr);
+    // useMosAlgorithm(n, c, arr);
+    useRandomize(n, c, arr);
   }
   static void useMosAlgorithm(int n, int c, int[] arr) throws IOException {
     ArrayList<Query> query = new ArrayList<>();
@@ -33,8 +34,7 @@ public class BOJ2912 {
       int lo = Integer.parseInt(st.nextToken());
       int hi = Integer.parseInt(st.nextToken());
       query.add(new Query(id, lo, hi));
-    }
-    
+    }    
     int k = (int) Math.sqrt(n);
     Collections.sort(query, new Comparator<Query>() {
       @Override
@@ -43,26 +43,85 @@ public class BOJ2912 {
       }
     });
     int[] count = new int[c+1];
-    int[] ncount = new int[n+1];
     int[] res = new int[numQuery];
-    int lo = 0, hi = 0, max = 0;
+    int lo = 0, hi = 0;
     for (Query q : query) {
-      while (hi < q.hi) {
-        int val = add(arr[++hi], count, ncount, max);
+      while (hi < q.hi) count[arr[++hi]]++;
+      while (lo > q.lo) count[arr[--lo]]++;
+      while (lo < q.lo) count[arr[lo++]]--;
+      while (hi > q.hi) count[arr[hi--]]--;
+      for (int color = 1; color <= c; color++) {
+        if (2*count[color] <= hi-lo+1) continue;
+        res[q.id] = color;
       }
-      while (lo > q.lo) add(arr[--lo], count, ncount, max);
-      while (lo < q.lo) sub(arr[lo++], count, ncount);
-      while (hi > q.hi) sub(arr[hi--], count, ncount);
+    }
+    for (int r : res) {
+      if (r == 0) {
+        bw.write("no");
+      } else {
+        bw.write("yes ");
+        bw.write(Integer.toString(r));
+      }
+      bw.newLine();
+    }
+    bw.flush();
+  }
+  static void useRandomize(int n, int c, int[] arr) throws IOException {
+    ArrayList<ArrayList<Integer>> ncolor = new ArrayList<>();
+    for (int i = 0; i <= c; i++) {
+      ncolor.add(new ArrayList<>());
+    }
+    for (int i = 1; i <= n; i++) {
+      ncolor.get(arr[i]).add(i);
     }
 
+    int numQuery = Integer.parseInt(br.readLine());
+    final int NUMLOOP = 15;
+    while (numQuery-- > 0) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int lo = Integer.parseInt(st.nextToken());
+      int hi = Integer.parseInt(st.nextToken());
+      int res = response(arr, ncolor, lo, hi, NUMLOOP);
+      if (res == -1) {
+        bw.write("no");
+      } else {
+        bw.write("yes ");
+        bw.write(Integer.toString(res));
+      }
+      bw.newLine();
+    }
+    bw.flush();
   }
-  static int add(int color, int[] count, int[] ncount, int max) {
-    if (count[color] != 0) ncount[count[color]]--;
-    count[color]++;
-    ncount[count[color]]++;
-    return Integer.max(max, count[color]);
+  static int response(int[] arr, ArrayList<ArrayList<Integer>> ncolor, int lo, int hi, int NUMLOOP) {
+    for (int i = 0; i < NUMLOOP; i++) {
+      int r = arr[(int)(Math.random()*(hi-lo+1) + lo)];
+      int val = upperbound(ncolor.get(r), hi) - lowerbound(ncolor.get(r), lo);
+      if (hi-lo+1 < 2*val) return r;
+    }
+    return -1;
   }
-  static void sub(int color, int[] count, int[] ncount) {
-
+  static int upperbound(ArrayList<Integer> arr, int key) {
+    int lo = -1, hi = arr.size();
+    while (lo+1 < hi) {
+      int mid = (lo+hi)>>1;
+      if (arr.get(mid) <= key) {
+        lo = mid;
+      } else {
+        hi = mid;
+      }
+    }
+    return hi;
+  }
+  static int lowerbound(ArrayList<Integer> arr, int key) {
+    int lo = -1, hi = arr.size();
+    while (lo+1 < hi) {
+      int mid = (lo+hi)>>1;
+      if (arr.get(mid) < key) {
+        lo = mid;
+      } else {
+        hi = mid;
+      }
+    }
+    return hi;
   }
 }
