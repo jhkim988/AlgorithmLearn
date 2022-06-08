2d3c0521d74261a03b7ec3e9b141adb42b46b438
jhKim988 <fccva3910@gmail.com>
2022-06-08 14:14:52 +0900

    Algorithm:SegmentTree+DynamicProgramming

--- Baekjoon/BOJ15648.java
+++ Baekjoon/BOJ15648.java
@@ -1,37 +1,36 @@
 import java.io.*;
 import java.util.*;
 
-public class BOJ17409 {
-  static final long D = 1_000_000_007;
+public class BOJ15648 {
   private static class SegTree {
     int n;
-    long[] sum;
+    long[] tree;
     SegTree(int n) {
       this.n = n;
       int treeSize = 1;
       while (treeSize < n) treeSize <<= 1;
       treeSize <<= 1;
-      sum = new long[treeSize];
+      tree = new long[treeSize];
     }
     void update(int node, int start, int end, int idx, long val) {
       if (idx < start || end < idx) return;
       if (start == end) {
-        sum[node] = val % D;
+        tree[node] = val;
         return;
       }
-      int mid = (start+end)>>1;
+      int mid = (start+end) >> 1;
       update(node<<1, start, mid, idx, val);
       update(node<<1|1, mid+1, end, idx, val);
-      sum[node] = (sum[node<<1] + sum[node<<1|1]) % D;
+      tree[node] = Long.max(tree[node<<1], tree[node<<1|1]);
     }
     long get(int node, int start, int end, int left, int right) {
       if (end < left || right < start) return 0;
-      if (left <= start && end <= right) return sum[node] % D;
-      int mid = (start+end)>>1;
-      return (get(node<<1, start, mid, left, right) + get(node<<1|1, mid+1, end, left, right)) % D;
+      if (left <= start && end <= right) return tree[node];
+      int mid = (start+end) >> 1;
+      return Long.max(get(node<<1, start, mid, left, right), get(node<<1|1, mid+1, end, left, right));
     }
     void update(int idx, long val) {
-      update(1, 0, n-1, idx, val % D);
+      update(1, 0, n-1, idx, val);
     }
     long get(int left, int right) {
       return get(1, 0, n-1, left, right);
@@ -43,26 +42,23 @@ public class BOJ17409 {
     StringTokenizer st = new StringTokenizer(br.readLine());
     int n = Integer.parseInt(st.nextToken());
     int k = Integer.parseInt(st.nextToken());
+    int d = Integer.parseInt(st.nextToken());
+    int[] arr = new int[n];
     st = new StringTokenizer(br.readLine());
-    int[] arr = new int[n+1];
-    for (int i = 1; i <= n; i++) {
+    for (int i = 0; i < n; i++) {
       arr[i] = Integer.parseInt(st.nextToken());
     }
-    SegTree[] sgs = new SegTree[k+1];
-    for (int l = 1; l <= k; l++) {
-      sgs[l] = new SegTree(n+1);
+    
+    long max = 0;
+    long[] remainder = new long[k];
+    SegTree sg = new SegTree(500_001);
+    for (int i = 0; i < n; i++) {
+      long val = Long.max(sg.get(arr[i]-d, arr[i]+d), remainder[arr[i]%k]) + 1;
+      if (max < val) max = val;
+      sg.update(arr[i], val);
+      remainder[arr[i]%k] = val;
     }
-    for (int i = 1; i <= n; i++) {
-      for (int l = 1; l <= k; l++) {
-        if (l == 1) {
-          sgs[l].update(arr[i], 1);
-        } else {
-          sgs[l].update(arr[i], sgs[l-1].get(0, arr[i]-1));
-        }
-      }
-    }
-    bw.write(Long.toString(sgs[k].get(1, n)));
-    bw.newLine();
+    bw.write(Long.toString(max));
     bw.flush();
   }
 }
