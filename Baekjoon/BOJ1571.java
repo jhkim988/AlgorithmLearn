6f5293682746f40083803aa220268d2b32c9f51b
jhKim988 <fccva3910@gmail.com>
2022-06-10 16:20:30 +0900

    Algorithm: Chinese Remainder Theorem

---
+++ Baekjoon/BOJ1571.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ1571 {
+  static final long D = 100_007; // 97 * 1031
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    int t = Integer.parseInt(br.readLine());
+    while (t-- > 0) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int n = Integer.parseInt(st.nextToken());
+      int m = Integer.parseInt(st.nextToken());
+      
+    }
+  }
+}
