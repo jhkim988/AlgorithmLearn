2dafe564535d3976b1ae2ddcc890eebcdc563390
jhkim988 <fccva3910@gmail.com>
2022-07-08 03:31:40 +0900

    ALgorithm

--- Baekjoon/BOJ10538.java
+++ Baekjoon/BOJ10538.java
@@ -2,14 +2,18 @@ import java.io.*;
 import java.util.*;
 
 public class BOJ10538 {
+  static long key = 31;
+  static long mod = 1_000_000_7;
+  static long rev = 129_032_259;
+  static int hp, wp, hm, wm;
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     StringTokenizer st = new StringTokenizer(br.readLine());
-    int hp = Integer.parseInt(st.nextToken());
-    int wp = Integer.parseInt(st.nextToken());
-    int hm = Integer.parseInt(st.nextToken());
-    int wm = Integer.parseInt(st.nextToken());
+    hp = Integer.parseInt(st.nextToken());
+    wp = Integer.parseInt(st.nextToken());
+    hm = Integer.parseInt(st.nextToken());
+    wm = Integer.parseInt(st.nextToken());
     char[][] pat = new char[hp][wp];
     for (int i = 0; i < hp; i++) {
       pat[i] = br.readLine().toCharArray();
@@ -19,49 +23,39 @@ public class BOJ10538 {
       str[i] = br.readLine().toCharArray();
     }
 
-    int[] piHorizon = new int[wp];
-    int[] piVertical = new int[hp];
-    
-    int j = 0;
-    for (int i = 1; i < wp; i++) {
-      while (j > 0 && equal(pat, 0, i, pat, 0, j, hp, true)) j = piHorizon[j-1];
-      if (equal(pat, 0, i, pat, 0, j, hp, true)) piHorizon[i] = ++j;
-    }
-    j = 0;
-    for (int i = 1; i < hp; i++) {
-      while (j > 0 && equal(pat, i, 0, pat, j, 0, wp, false)) j = piVertical[j-1];
-      if (equal(pat, i, 0, pat, j, 0, wp, false)) piVertical[i] = ++j;
-    }
-
-    int num = 0;
-    for (int i = 0; i <= hm-hp; i++) {
-      j = 0;
-      for (int jj = 0; jj < wm; jj++) {
-        while (j > 0 && !equal(str, i, jj, pat, 0, j, hp)) j = pi[j-1];
-        if (equal(str, i, jj, pat, 0, j, hp)) {
-          if (j == wp-1) {
-            j = pi[j];
-            num++;
-          } else {
-            j++;
-          }
-        }
+    long[] hpat = new long[hp];
+    for (int i = 0; i < hp; i++) {
+      long exp = 1;
+      for (int j = 0; j < wp; j++) {
+        hpat[i] += pat[i][j]*exp%mod; hpat[i] %= mod;
+        exp = exp*key%mod;
       }
     }
 
-    bw.write(Integer.toString(num));
+    long[][] hstr = new long[hm][wm-wp+1];
+    for (int i = 0; i < hm; i++) {
+      long exp = 1;
+      for (int j = 0; j < wp; j++) {
+        hstr[i][0] += str[i][j]*exp%mod; hstr[i][0] %= mod;
+        exp = exp*key%mod;        
+      }
+    }
+    long exp = 1;
+    for (int i = 0; i < wp; i++) exp = exp*key%mod;
+    for (int i = 0; i < hm; i++) {
+      for (int j = 1; j <= wm-wp; j++) {
+        long val = hstr[i][j-1];
+        val += (mod-str[i][j-1])%mod; val %= mod;
+        val += str[i][j-1+wp]*exp; val %= mod;
+        hstr[i][j] = val*rev%mod;
+      }
+    }
+
+    System.out.println(Arrays.toString(hpat));
+    for (int i = 0; i < hm; i++) {
+      System.out.println(Arrays.toString(hstr[i]));
+    }
+    bw.write(Integer.toString(0));
     bw.flush();
-  }
-  static boolean equal(char[][] a, int arow, int acol, char[][] b, int brow, int bcol, int n, boolean isVertical) {
-    if (isVertical) {
-      for (int i = 0; i < n; i++) {
-        if (a[arow+i][acol] != b[brow+i][bcol]) return false;
-      }
-    } else {
-      for (int i = 0; i < n; i++) {
-        if (a[arow][acol+i] != a[brow][bcol+i]) return false;
-      }
-    }
-    return true;
   }
 }
