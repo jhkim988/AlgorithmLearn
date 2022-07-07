1f713b4a5ae8cd696b6e81af3062c90ac9196484
jhKim988 <fccva3910@gmail.com>
2022-07-07 16:22:17 +0900

    Algorithm: KMP

--- Baekjoon/BOJ10538.java
+++ Baekjoon/BOJ10538.java
@@ -19,11 +19,18 @@ public class BOJ10538 {
       str[i] = br.readLine().toCharArray();
     }
 
-    int[] pi = new int[wp];
+    int[] piHorizon = new int[wp];
+    int[] piVertical = new int[hp];
+    
     int j = 0;
     for (int i = 1; i < wp; i++) {
-      while (j > 0 && !equal(pat, 0, i, pat, 0, j, hp)) j = pi[j-1];
-      if (equal(pat, 0, i, pat, 0, j, hp)) pi[i] = ++j;
+      while (j > 0 && equal(pat, 0, i, pat, 0, j, hp, true)) j = piHorizon[j-1];
+      if (equal(pat, 0, i, pat, 0, j, hp, true)) piHorizon[i] = ++j;
+    }
+    j = 0;
+    for (int i = 1; i < hp; i++) {
+      while (j > 0 && equal(pat, i, 0, pat, j, 0, wp, false)) j = piVertical[j-1];
+      if (equal(pat, i, 0, pat, j, 0, wp, false)) piVertical[i] = ++j;
     }
 
     int num = 0;
@@ -45,9 +52,15 @@ public class BOJ10538 {
     bw.write(Integer.toString(num));
     bw.flush();
   }
-  static boolean equal(char[][] a, int arow, int acol, char[][] b, int brow, int bcol, int n) {
-    for (int i = 0; i < n; i++) {
-      if (a[arow+i][acol] != b[brow+i][bcol]) return false;
+  static boolean equal(char[][] a, int arow, int acol, char[][] b, int brow, int bcol, int n, boolean isVertical) {
+    if (isVertical) {
+      for (int i = 0; i < n; i++) {
+        if (a[arow+i][acol] != b[brow+i][bcol]) return false;
+      }
+    } else {
+      for (int i = 0; i < n; i++) {
+        if (a[arow][acol+i] != a[brow][bcol+i]) return false;
+      }
     }
     return true;
   }
