43f32eee1e0268a6d4d6368cf8f51170fc02e5cc
jhKim988 <fccva3910@gmail.com>
2022-07-13 10:24:41 +0900

    Algorithm: BST, BBST(Treap) Implementation

--- Implementation/Tree/Treap.java
+++ Implementation/Tree/Treap.java
@@ -1,3 +1,48 @@
+import java.io.*;
+
 public class Treap {
-  
+  static final int INF = Integer.MAX_VALUE/2;
+  private static class Node {
+    int weight, key;
+    Node leftChild, rightChild;
+    Node(int key) {
+      this.key = key;
+      weight = (int) (INF*Math.random());
+    }
+  }
+  // weight: MaxHeap
+  // key: BinarySearchTree
+  Node root;
+  void add(int key) {
+    add(root, new Node(key));
+  }
+  Node find(int key) {
+    return null;
+  }
+  Node delete(int key) {
+    return null;
+  }
+  void add(Node ptr, Node node) {
+    if (ptr == null) {
+      ptr = node;
+      return;
+    }
+    if (ptr.weight > node.weight) {
+      if (ptr.key > node.key) {
+        add(ptr.leftChild, node);
+      } else if (ptr.key < node.key) {
+        add(ptr.rightChild, node);
+      } else {
+        // duplicate key:
+      }
+    } else {
+      if (ptr.key > node.key) {
+
+      } else if (ptr.key < node.key) {
+
+      } else {
+
+      }
+    }
+  }
 }
