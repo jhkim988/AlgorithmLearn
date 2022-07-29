a590621fd02e40c191da70b942948915bab529c6
jhKim988 <fccva3910@gmail.com>
2022-07-29 16:05:31 +0900

    Algorithm Implementation: Sort Test / Accuracy Test

--- Implementation/Sort/SortTest.java
+++ Implementation/Sort/SortTest.java
@@ -1,8 +1,9 @@
 import java.util.*;
 
+@SuppressWarnings("unchecked")
 public class SortTest {
-  Sort sort;
-  SortTest(Sort sort) {
+  Sort<Integer> sort;
+  SortTest(Sort<Integer> sort) {
     this.sort = sort;
   }
   void timeTest() {
@@ -17,12 +18,28 @@ public class SortTest {
     }
     return arrList;
   }
+  boolean accuracy() {
+    ArrayList<Integer> ret = (ArrayList<Integer>) sort.get();
+    for (int i = 0; i < ret.size()-1; i++) {
+      if (ret.get(i) > ret.get(i+1)) return false;
+    }
+    return true;
+  }
+  void accuracyTest() {
+    System.out.printf("Accuracy Test: %s\n", accuracy() ? "PASS" : "FAIL");
+  }
   public static void main(String[] args) {
     int numData = 1_000_000;
     ArrayList<Integer> arrList = makeRandom(numData);
     SortTest quickSort = new SortTest(new QuickSort<Integer>((ArrayList<Integer>) arrList.clone()));
     SortTest dualPivot = new SortTest(new DualPivotQuickSort<Integer>((ArrayList<Integer>) arrList.clone()));
+    SortTest mergeSort = new SortTest(new MergeSort<Integer>((ArrayList<Integer>) arrList.clone()));
     quickSort.timeTest();
     dualPivot.timeTest();
+    mergeSort.timeTest();
+
+    quickSort.accuracyTest();
+    dualPivot.accuracyTest();
+    mergeSort.accuracyTest();
   }
 }
