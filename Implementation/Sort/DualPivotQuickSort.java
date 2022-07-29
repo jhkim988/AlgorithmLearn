4acee75950d0c410e3a4a528f22f8f4ced158814
jhKim988 <fccva3910@gmail.com>
2022-07-29 15:32:52 +0900

    Algorithm Implementation: DualPivot QuickSort

---
+++ Implementation/Sort/DualPivotQuickSort.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class DualPivotQuickSort <T extends Comparable<? super T>> implements Sort {
+  ArrayList<T> arrList;
+  DualPivotQuickSort(ArrayList<T> arrList) {
+    this.arrList = arrList;
+  }
+  public void sort() {
+    sort(0, arrList.size()-1);
+  }
+  public void sort(int lo, int hi) {
+    if (hi <= lo) return;
+    int left = lo, right = hi;
+    
+    if (arrList.get(left).compareTo(arrList.get(right)) > 0) swap(left, right);
+    T leftPivot = arrList.get(left), rightPivot = arrList.get(right);
+    for (int ptr = left+1; ptr <= right-1; ptr++) {
+      if (arrList.get(ptr).compareTo(leftPivot) < 0) {
+        if (ptr-left == 1) continue;
+        swap(left, left+1);
+        swap(left, ptr);
+        left++;
+      } else if (arrList.get(ptr).compareTo(rightPivot) >= 0) {
+        if (right-ptr == 1) continue;
+        swap(right-1, right);
+        swap(right, ptr);
+        right--;
+      }
+    }
+    sort(lo, left-1);
+    sort(left+1, right-1);
+    sort(right+1, hi);
+  }
+  private void swap(int a, int b) {
+    T tmp = arrList.get(a);
+    arrList.set(a, arrList.get(b));
+    arrList.set(b, tmp);
+  }
+  void print() {
+    for (T v : arrList) {
+      System.out.print(v + " ");
+    }
+    System.out.println();
+  }
+  public static void main(String[] args) {
+    int numData = 10;
+    ArrayList<Integer> arrList = new ArrayList<>();
+    for (int i = 0; i < numData; i++) {
+      arrList.add((int) (Math.random()*1000));
+    }
+    DualPivotQuickSort<Integer> dualPivot = new DualPivotQuickSort<>(arrList);
+    dualPivot.sort();
+    dualPivot.print();
+  }
+}
