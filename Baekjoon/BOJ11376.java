5ca1abc2aff427f4fd679de5c1de2bd0a267564a
jhkim988 <fccva3910@gmail.com>
2022-09-14 19:58:32 +0900

    Algorithm: Binary Matching

--- Baekjoon/BOJ11376.java
+++ Baekjoon/BOJ11376.java
@@ -2,6 +2,9 @@ import java.io.*;
 import java.util.*;
 
 public class BOJ11376 {
+  static int[][] graph;
+  static boolean[] visit;
+  static int[] employee, work;
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -9,38 +12,37 @@ public class BOJ11376 {
     int n = Integer.parseInt(st.nextToken());
     int m = Integer.parseInt(st.nextToken());
     
-    ArrayList<Queue<Integer>> graph = new ArrayList<>();
-    int[] employee = new int[n+1];
-    int[] work = new int[m+1];
+    graph = new int[2*n+1][];
+    employee = new int[2*n+1];
+    work = new int[m+1];
     
-    for (int i = 0; i <= n; i++) graph.add(new LinkedList<>());
     for (int i = 1; i <= n; i++) {
       st = new StringTokenizer(br.readLine());
       int w = Integer.parseInt(st.nextToken());
-      while (w-- > 0) {
+      graph[i*2-1] = new int[w];
+      graph[i*2] = new int[w];
+      for (int j = 0; j < w; j++) {
         int input = Integer.parseInt(st.nextToken());
-        graph.get(i).add(input);
+        graph[i*2-1][j] = graph[i*2][j] = input; 
       }
     }
 
     int numMatch = 0;
-    boolean[] visit = new boolean[n+1];
-    for (int i = 1; i <= n; i++) {
+    visit = new boolean[2*n+2];
+    for (int i = 1; i < 2*n+1; i++) {
       if (employee[i] != 0) continue;
       Arrays.fill(visit, false);
-      if (dfs(i, graph, visit, employee, work)) numMatch++;
-      Arrays.fill(visit, false);
-      if (dfs(i, graph, visit, employee, work)) numMatch++;
+      if (dfs(i)) numMatch++;
     }
 
     bw.write(Integer.toString(numMatch));
     bw.newLine();
     bw.flush();
   }
-  static boolean dfs(int a, ArrayList<Queue<Integer>> graph, boolean[] visit, int[] employee, int[] work) {
+  static boolean dfs(int a) {
     visit[a] = true;
-    for (int b : graph.get(a)) {
-      if (work[b] == 0 || (!visit[work[b]] && dfs(work[b], graph, visit, employee, work))) {
+    for (int b : graph[a]) {
+      if (work[b] == 0 || (!visit[work[b]] && dfs(work[b]))) {
         employee[a] = b;
         work[b] = a;
         return true;
---
+++ Baekjoon/BOJ11377.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ11377 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int n = Integer.parseInt(st.nextToken());
+    int m = Integer.parseInt(st.nextToken());
+    int k = Integer.parseInt(st.nextToken());
+    
+  }
+}
