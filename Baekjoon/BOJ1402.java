ccf92cb8ce95f3fcd26f8dab5e201b83c1436505
jhkim988 <fccva3910@gmail.com>
2022-09-17 23:24:38 +0900

    Algorithm: Trick

---
+++ Baekjoon/BOJ1402.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class BOJ1402 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int numTest = Integer.parseInt(br.readLine());
+    while (numTest-- > 0) {
+      System.out.println("yes");
+    }
+  }
+}
