8cdcbb24f23b1ae34300f7bcb1a8a8e5be402446
jhkim988 <fccva3910@gmail.com>
2022-10-30 23:28:43 +0900

    Algorithm: BFS Optimize

--- Baekjoon/BOJ5214.java
+++ Baekjoon/BOJ5214.java
@@ -2,8 +2,6 @@ import java.io.*;
 import java.util.*;
 
 public class BOJ5214 {
-    private static int n, m;
-    private static List<HashSet<Integer>> stationToGroupId, groupIdToStation;
     private static class Pair {
         int end, count;
         Pair(int end, int count) {
@@ -15,54 +13,40 @@ public class BOJ5214 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        stationToGroupId = new ArrayList<>();
-        groupIdToStation = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
-            stationToGroupId.add(new HashSet<>());
+        List<Queue<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i <= n+m; i++) {
+            graph.add(new LinkedList<>());
         }
-        for (int i = 0; i <= m; i++) {
-            groupIdToStation.add(new HashSet<>());
-        }
-
-        for (int id = 1; id <= m; id++) {
+        for (int i = n+1; i <= n+m; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < k; j++) {
-                int station = Integer.parseInt(st.nextToken());
-                stationToGroupId.get(station).add(id);
-                groupIdToStation.get(id).add(station);
+                int idx = Integer.parseInt(st.nextToken());
+                graph.get(idx).add(i);
+                graph.get(i).add(idx);
             }
         }
 
-        int count = bfs();
+        int count = bfs(graph, n, m);
         bw.write(Integer.toString(count));
         bw.flush();
     }
-    private static int bfs() {
+    private static int bfs(List<Queue<Integer>> graph, int n, int m) {
         if (n == 1) return 1;
-        boolean[] visitGroup = new boolean[m+1];
-        boolean[] visitNode = new boolean[n+1];
+        boolean[] visit = new boolean[n+m+1];
         Queue<Pair> que = new LinkedList<>();
-        for (int id : stationToGroupId.get(1)) {
-            que.add(new Pair(id, 1));
-            visitGroup[id] = true;
-        }
-        visitNode[1] = true;
-    
+        que.add(new Pair(1, 1));
+        visit[1] = true;
         while (!que.isEmpty()) {
             Pair crnt = que.poll();
-            for (int station : groupIdToStation.get(crnt.end)) {
-                if (visitNode[station]) continue;
-                if (station == n) return crnt.count+1;
-                visitNode[station] = true;
-                for (int id : stationToGroupId.get(station)) {
-                    if (visitGroup[id]) continue;
-                    visitGroup[id] = true;
-                    que.add(new Pair(id, crnt.count+1));
-                }
+            for (int adj : graph.get(crnt.end)) {
+                if (visit[adj]) continue;
+                if (adj == n) return crnt.count+1;
+                visit[adj] = true;
+                que.add(new Pair(adj, crnt.count + (adj <= n ? 1 : 0)));
             }
         }
         return -1;
