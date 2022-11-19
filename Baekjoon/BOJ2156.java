13ed5d1df205de66146e73b2c755bafe8287a113
jhkim988 <fccva3910@gmail.com>
2022-11-19 09:25:40 +0900

    Algorithm: DP TopDown Review

--- Baekjoon/BOJ2156.java
+++ Baekjoon/BOJ2156.java
@@ -1,37 +1,41 @@
 import java.io.*;
+import java.util.*;
 
 public class BOJ2156 {
-  public static void main(String[] args) {
-    try {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-      int numWine = Integer.parseInt(br.readLine());
-      int[][] sumData = new int[numWine][3];
-      int tmp;
-      for (int i = 0; i < numWine; i++) {
-        tmp = Integer.parseInt(br.readLine());
-        if (i == 0) {
-          sumData[i][0] = tmp;
-        } else if (i == 1) {
-          sumData[i][0] = sumData[i - 1][0] + tmp;
-          sumData[i][1] = tmp;
-          sumData[i][2] = sumData[i - 1][0];
-        } else if (i == 2) {
-          sumData[i][0] = sumData[i - 1][1] + tmp;
-          sumData[i][1] = Math.max(sumData[i - 2][0], sumData[i - 2][1]) + tmp;
-          sumData[i][2] = sumData[i - 1][0];
-        } else {
-          sumData[i][0] = Math.max(sumData[i - 1][1], sumData[i - 1][2]) + tmp;
-          sumData[i][1] = Math.max(sumData[i - 2][0], Math.max(sumData[i - 2][1], sumData[i - 2][2])) + tmp;
-          sumData[i][2] = Math.max(sumData[i - 1][0], Math.max(sumData[i - 1][1], sumData[i - 1][2]));
-        }
-      }
-
-      bw.write(Math.max(sumData[numWine - 1][0], Math.max(sumData[numWine - 1][1], sumData[numWine - 1][2])) + "\n");
-      bw.flush();
-    } catch (IOException e) {
-      // do nothing
+  private static int n;
+  private static int[] arr;
+  private static int[][] dp;
+  private static final int INF = Integer.MAX_VALUE >> 1;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    n = Integer.parseInt(br.readLine());
+    arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(br.readLine());
     }
+    dp = new int[3][n];
+    for (int i = 0; i < 3; i++) {
+      Arrays.fill(dp[i], -1);
+    }
+    int max = 0;
+    for (int i = 0; i < 3; i++) {
+      max = Integer.max(max, recur(0, i));
+    }
+    bw.write(Integer.toString(max));
+    bw.flush();
+  }
+  private static int recur(int pos, int numConti) {
+    if (pos >= n) return 0;
+    if (numConti < 0) return -INF;
+    if (dp[numConti][pos] != -1) return dp[numConti][pos];
+    if (numConti == 0) {
+      return dp[numConti][pos] = Integer.max(recur(pos+1, 0), Integer.max(recur(pos+1, 1), recur(pos+1, 2)));
+    }
+    int sum = 0;
+    for (int i = 0; i < numConti && pos+i < n; i++) {
+      sum += arr[pos+i];
+    }
+    return dp[numConti][pos] = recur(pos + numConti, 0) + sum;
   }
 }
