c682e39830ffc25b17e37e0b39a5fb55e5674abc
jhkim <jinhankim988@gmail.com>
2023-02-10 08:53:54 +0900

    eratosthenes

--- Baekjoon/BOJ6588.java
+++ Baekjoon/BOJ6588.java
@@ -2,57 +2,45 @@ import java.io.*;
 import java.util.*;
 
 public class BOJ6588 {
-  // String.format(): SLOW!
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    ArrayList<Integer> primes = new ArrayList<>();
-    int limit = 10;
-    addPrime(primes, limit);
-    int num = Integer.parseInt(br.readLine());
-    StringBuilder sb = new StringBuilder();
-    while (num != 0) {
-      int ptr1 = 0;
-      int ptr2 = primes.size() - 1;
-      while (limit < num) {
-        limit *= 10;
-        addPrime(primes, limit);
-        ptr2 = primes.size() - 1;
-      }
-      while (ptr1 < ptr2) {
-        int ptr1Val = primes.get(ptr1);
-        int ptr2Val = primes.get(ptr2);
-        int val = ptr1Val + ptr2Val;
-        if (val < num) {
-          ptr1++;
-        } else if (val > num) {
-          ptr2--;
-        } else {
-          sb.append(num).append(" = ").append(ptr1Val).append(" + ").append(ptr2Val).append("\n");
-          break;
-        }
-      }
-      num = Integer.parseInt(br.readLine());
-    } 
-    bw.write(sb.toString());
-    bw.flush();
-  }  
-  static void addPrime(ArrayList<Integer> primes, int limit) {
-    if (primes.size() == 0) {
-      primes.add(3);
-    }
-    int last = primes.get(primes.size() - 1);
-    if (last >= limit) return;
-    for (int i = last; i <= limit; i++) {
-      if (isPrime(i)) {
-        primes.add(i);
-      }
-    }
-  }
-  static boolean isPrime(int x) {
-    for (int i = 2; i * i <= x; i++) {
-      if (x % i == 0) return false;
-    }
-    return true;
-  }
+	private static class Prime {
+		boolean[] isPrime;
+
+		Prime(int n) {
+			isPrime = new boolean[n];
+			Arrays.fill(isPrime, true);
+			isPrime[0] = isPrime[1] = false;
+			for (int i = 2; i*i <= n; i++) {
+				if (!isPrime[i]) continue;
+				for (int j = i<<1; j < n; j += i) {
+					isPrime[j] = false;
+				}
+			}
+		}
+
+		boolean isPrime(int x) {
+			return isPrime[x];
+		}
+	}
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		Prime p = new Prime(1_000_001);
+
+		StringBuilder sb = new StringBuilder();
+		test: for (int num = Integer.parseInt(br.readLine()); num != 0; num = Integer.parseInt(br.readLine())) {
+			int ptr1 = 3;
+			int ptr2 = num-3;
+			while (ptr1 <= ptr2) {
+				if (p.isPrime(ptr1) && p.isPrime(ptr2)) {
+					sb.append(num).append(" = ").append(ptr1).append(" + ").append(ptr2).append('\n');
+					continue test;
+				}
+				ptr1 += 2;
+				ptr2 -= 2;
+			}
+		}
+		
+		bw.write(sb.toString());
+		bw.flush();
+	}
 }
